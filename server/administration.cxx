476fe6252f98c8d218205ff817312e6be2193e64
Jaemin Ryu <jm77.ryu@samsung.com>
2015-12-24 11:37:40 +0900

    Add administration policies
    Signed-off-by: Jaemin Ryu <jm77.ryu@samsung.com>

---
+++ server/administration.cxx
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#include "administration.hxx"
+#include "audit/logger.hxx"
+
+namespace DevicePolicyManager {
+
+Administration::Administration(PolicyControlContext& ctx)
+    : context(ctx)
+{
+    Ipc::Service& manager = context.getServiceManager();
+
+    manager.registerParametricMethod(this, (int)(Administration::registerPolicyClient)(std::string));
+    manager.registerParametricMethod(this, (int)(Administration::deregisterPolicyClient)(std::string));
+}
+
+Administration::~Administration()
+{
+}
+
+int Administration::registerPolicyClient(const std::string& name)
+{
+    DevicePolicyServer::ClientManager &manager = context.getClientManager();
+
+    try {
+        manager.registerClient(name);
+    } catch (Runtime::Exception& e) {
+        ERROR("Failed to register policy client");
+        return -1;
+    }
+
+    return 0;
+}
+
+int Administration::deregisterPolicyClient(const std::string& name)
+{
+    DevicePolicyServer::ClientManager& manager = context.getClientManager();
+
+    try {
+        manager.deregisterClient(name);
+    } catch (Runtime::Exception& e) {
+        ERROR("Failed to deregister policy client");
+        return -1;
+    }
+
+    return 0;
+}
+
+Administration adminPolicy(DevicePolicyServer::Server::instance());
+
+} // namespace DevicePolicyManager
