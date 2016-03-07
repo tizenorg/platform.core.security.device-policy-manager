cf69ae5b923d7dfc4a7d0cf685cd72e41fbd8972
Sungbae Yoo <sungbae.yoo@samsung.com>
2016-03-07 19:27:08 +0900

    Add container policies
    Change-Id: Ib3f46c7229f0ff53b6ad7cef7ab16e6dc1b38b38
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

---
+++ libs/container.cxx
@@ -0,0 +1,78 @@
+/*
+ *  Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#include "policy-client.hxx"
+
+#include "container.hxx"
+
+namespace DevicePolicyManager {
+
+Container::Container(PolicyControlContext& ctx)
+    : context(ctx)
+{
+}
+
+Container::~Container()
+{
+}
+
+int Container::create(const std::string& name, const std::string& setupWizAppid)
+{
+    try {
+        return context->methodCall<int>("Container::create", name, setupWizAppid);
+    } catch (Runtime::Exception& e) {
+        return -1;
+    }
+}
+
+int Container::remove(const std::string& name)
+{
+    try {
+        return context->methodCall<int>("Container::remove", name);
+    } catch (Runtime::Exception& e) {
+        return -1;
+    }
+}
+
+int Container::lock(const std::string& name)
+{
+    try {
+        return context->methodCall<int>("Container::lock", name);
+    } catch (Runtime::Exception& e) {
+        return -1;
+    }
+}
+
+int Container::unlock(const std::string& name)
+{
+    try {
+        return context->methodCall<int>("Container::unlock", name);
+    } catch (Runtime::Exception& e) {
+        return -1;
+    }
+}
+
+Vector<String> Container::getList()
+{
+    Vector<String> empty;
+    try {
+        return  context->methodCall<Vector<String>>("Container::getList");
+    } catch (Runtime::Exception& e) {
+        return empty;
+    }
+}
+
+} // namespace DevicePolicyManager
