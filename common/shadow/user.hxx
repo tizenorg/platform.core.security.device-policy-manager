efb103f2e4aa1d5eb6da057cb08c33b761844bb1
Sungbae Yoo <sungbae.yoo@samsung.com>
2016-03-07 19:16:26 +0900

    Add support for multi-user using by shadow system
    Change-Id: Ib5fb4949e63b9cacb3c198eebb27ee9d9f7bc46f
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

---
+++ common/shadow/user.hxx
@@ -0,0 +1,71 @@
+/*
+ *  Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#ifndef __SHADOW_USER__
+#define __SHADOW_USER__
+
+#include <string>
+#include <limits.h>
+#include <sys/types.h>
+
+#ifndef INVALID_UID
+#define INVALID_UID             UINT_MAX
+#endif
+
+#ifndef INVALID_GID
+#define INVALID_GID             UINT_MAX
+#endif
+
+namespace Shadow  {
+
+class User final {
+public:
+    const std::string& getName() const {
+        return name;
+    }
+    const std::string& getGroup() {
+        return group;
+    }
+
+    const uid_t getUid() const {
+        return uid;
+    }
+    const gid_t getGid() const {
+        return gid;
+    }
+
+    const std::string& getShell() {
+        return shell;
+    }
+
+    static User create(const std::string& name, const std::string& group,
+                       const uid_t min = 5000, const uid_t max = 65000);
+    void remove();
+
+    User(const std::string& name);
+    User(const User& user);
+
+private:
+    std::string name, group;
+    uid_t uid;
+    gid_t gid;
+
+    std::string shell;
+};
+
+} // namespace Shadow
+
+#endif //_SHADOW_USER__
