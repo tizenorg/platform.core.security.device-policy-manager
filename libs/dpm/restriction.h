1c20fe5f35c149d70268c27099780a2e8bf021e0
seolhee, kim <s414.kim@samsung.com>
2016-03-14 20:25:23 +0900

    Add restriction policies
    Change-Id: I08b1e2151f13ab0218e5d80749d9e42611ef7ce9

---
+++ libs/dpm/restriction.h
@@ -0,0 +1,343 @@
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#ifndef __CAPI_RESTRICTION_POLICY_H__
+#define __CAPI_RESTRICTION_POLICY_H__
+
+#include <dpm/device-policy-client.h>
+
+/**
+ * @file restriction.h
+ * @brief This file provides APIs to control restriction functionality
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @addtogroup  DPM_RESTRICTION_POLICY
+ * @{
+ */
+
+/**
+ * @brief       API to enable or disable the clipboard
+ * @details     An administrator can use this API to set whether the clipboard
+ *              is allowed or not
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @param[in]   enable If true, enable the clipboard, if false, disable the clipboard
+ * @return      #DPM_ERROR_NONE on success, otherwise a negative value
+ * @retval      #DPM_ERROR_NONE Successful
+ * @retval      #DPM_ERROR_NOT_SUPPORTED Not supported
+ * @retval      #DPM_ERROR_ACCESS_DENIED The application does not have
+ *              the privilege to call this API
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API int dpm_set_allow_clipboard(dpm_client_h handle, const bool enable);
+
+/**
+ * @brief       API to get the allow status of the clipboard
+ * @details     An administrator can use this API to get the allow status of
+ *              the clipboard
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @return      true if the clipboard is allowed, else false
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API bool dpm_get_allow_clipboard(dpm_client_h handle);
+
+/**
+ * @brief       API to enable or disable the settings changes
+ * @details     An administrator can use this API to set whether the Settins
+ *              changes is allowed or not
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @param[in]   enable If true, enable the settings changes, if false, disable
+ *              the settings changes
+ * @return      #DPM_ERROR_NONE on success, otherwise a negative value
+ * @retval      #DPM_ERROR_NONE Successful
+ * @retval      #DPM_ERROR_NOT_SUPPORTED Not supported
+ * @retval      #DPM_ERROR_ACCESS_DENIED The application does not have
+ *              the privilege to call this API
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API int dpm_set_allow_settings_changes(dpm_client_h handle, const bool enable);
+
+/**
+ * @brief       API to get the allow status of the settings changes
+ * @details     An administrator can use this API to get the allow statsus of
+ *              the Settins changes
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @return      true if the settings changes is allowed, else false
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API bool dpm_get_allow_settings_changes(dpm_client_h handle);
+
+/**
+ * @brief       API to enable or disable the background data
+ * @details     An administrator can use this API to set whether the background
+ *              data is allowed or not
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @param[in]   enable If true, enable the background data, if false, disable
+ *              the background data
+ * @return      #DPM_ERROR_NONE on success, otherwise a negative value
+ * @retval      #DPM_ERROR_NONE Successful
+ * @retval      #DPM_ERROR_NOT_SUPPORTED Not supported
+ * @retval      #DPM_ERROR_ACCESS_DENIED The application does not have
+ *              the privilege to call this API
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API int dpm_set_allow_background_data(dpm_client_h handle, const bool enable);
+
+/**
+ * @brief       API to get the allow status of the background data
+ * @details     An administrator can use this API to get the allow status of
+ *              the background data
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @return      true if the background data is allowed, else false
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API bool dpm_get_allow_background_data(dpm_client_h handle);
+
+/**
+ * @brief       API to enable or disable the usb debugging
+ * @details     An administrator can use this API to set whether the usb debugging
+ *              is allowed or not
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @param[in]   enable If true, enable the usb debugging, if false, disable the usb
+ *              debugging
+ * @return      #DPM_ERROR_NONE on success, otherwise a negative value
+ * @retval      #DPM_ERROR_NONE Successful
+ * @retval      #DPM_ERROR_NOT_SUPPORTED Not supported
+ * @retval      #DPM_ERROR_ACCESS_DENIED The application does not have
+ *              the privilege to call this API
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API int dpm_set_allow_usb_debugging(dpm_client_h handle, const bool enable);
+
+/**
+ * @brief       API to get the allow status of the usb debugging
+ * @details     An administrator can use this API to get the allow status of
+ *              the usb debugging
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @return      true if the usb debugging is allowed, else false
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API bool dpm_get_allow_usb_debugging(dpm_client_h handle);
+
+/**
+ * @brief       API to enable or disable the usb mass storage
+ * @details     An administrator can use this API to set whether the usb mass
+ *              storage is allowed or not
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @param[in]   enable If true, enable the usb mass storage, if false, disable
+ *              the usb mas storage
+ * @return      #DPM_ERROR_NONE on success, otherwise a negative value
+ * @retval      #DPM_ERROR_NONE Successful
+ * @retval      #DPM_ERROR_NOT_SUPPORTED Not supported
+ * @retval      #DPM_ERROR_ACCESS_DENIED The application does not have
+ *              the privilege to call this API
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API int dpm_set_allow_usb_mass_storage(dpm_client_h handle, const bool enable);
+
+/**
+ * @brief       API to get the allow status of the usb mass storage
+ * @details     An administrator can use this API to get the allow status of
+ *              the usb mass storage
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @return      true if the usb mass storage is allowed, else false
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API bool dpm_get_allow_usb_mass_storage(dpm_client_h handle);
+
+/**
+ * @brief       API to enable or disable the factory reset
+ * @details     An administrator can use this API to set whether the factory
+ *              reset is allowed or not
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @param[in]   enable If true, enable the factory reset, if false, disbale
+ *              the factory reset
+ * @return      #DPM_ERROR_NONE on success, otherwise a negative value
+ * @retval      #DPM_ERROR_NONE Successful
+ * @retval      #DPM_ERROR_NOT_SUPPORTED Not supported
+ * @retval      #DPM_ERROR_ACCESS_DENIED The application does not have
+ *              the privilege to call this API
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API int dpm_set_allow_factory_reset(dpm_client_h handle, const bool enable);
+
+/**
+ * @brief       API to get the allow status of the factory reset
+ * @details     An administrator can use this API to get the allow status of
+ *              factory reset
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @return      true if the factory reset is allowed, else false
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API bool dpm_get_allow_factory_reset(dpm_client_h handle);
+
+/**
+ * @brief       API to enable or disable the bluetooth tethering
+ * @details     An administrator can use this API to set whether the bluetooth
+ *              tethering is allowed or not
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @param[in]   enable If true, enable the bluetooth tethering, if false, disable
+ *              the bluetooth tethering
+ * @return      #DPM_ERROR_NONE on success, otherwise a negative value
+ * @retval      #DPM_ERROR_NONE Successful
+ * @retval      #DPM_ERROR_NOT_SUPPORTED Not supported
+ * @retval      #DPM_ERROR_ACCESS_DENIED The application does not have
+ *              the privilege to call this API
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API int dpm_set_allow_bluetooth_tethering(dpm_client_h handle, const bool enable);
+
+/**
+ * @brief       API to get the allow status of the bluetooth tethering
+ * @details     An administrator can use this API to get the allow status of
+ *              the bluetooth tethering
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @return      true if the bluetooth tethering is allowed, else false
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API bool dpm_get_allow_bluetooth_tethering(dpm_client_h handle);
+
+/**
+ * @brief       API to enable or disable the mock location
+ * @details     An administrator can use this API to set whether the mock location
+ *              is allowed or not
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @param[in]   enable If true, enable the mock location, if false, disable the mock
+ *              location
+ * @return      #DPM_ERROR_NONE on success, otherwise a negative value
+ * @retval      #DPM_ERROR_NONE Successful
+ * @retval      #DPM_ERROR_NOT_SUPPORTED Not supported
+ * @retval      #DPM_ERROR_ACCESS_DENIED The application does not have
+ *              the privilege to call this API
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API int dpm_set_allow_mock_location(dpm_client_h handle, const bool enable);
+
+/**
+ * @brief       API to get the allow status of the mock location
+ * @details     An administrator can use this API to get the allow status of
+ *              the mock location
+ * @since_tizen 3.0
+ * @privlevel   public
+ * @privilege   %http://tizen.org/privilege/dpm.restriction
+ * @param[in]   handle Device Policy Client handle
+ * @return      true if the mock location is allowed, else false
+ * @pre         handle must be created by dpm_create_client()
+ * @post
+ * @see         dpm_create_client()
+ * @see         dpm_destroy_client()
+ */
+DPM_API bool dpm_get_allow_mock_location(dpm_client_h handle);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __CAPI_RESTRICTION_POLICY_H__ */
