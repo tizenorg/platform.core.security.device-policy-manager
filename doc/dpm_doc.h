7d909452c64b2d851f5ef0ac44ad7d3699d4d732
Jaemin Ryu <jm77.ryu@samsung.com>
2016-03-18 18:59:09 +0900

    Add doxygen document
    Change-Id: Ifa787063612460a3485f1d8222aed8f3f408613d
    Signed-off-by: Jaemin Ryu <jm77.ryu@samsung.com>

---
+++ doc/dpm_doc.h
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#ifndef __DPM_DOC_H__
+#define __DPM_DOC_H__
+
+
+ /**
+  *
+  *  @defgroup DPM_POLICY_CLIENT Policy Client Interface
+  *  @brief Client interface to access policy APIs.
+  *
+  *  @ingroup DPM_POLICY_CLIENT
+  *  All APIs of this group are defined in dpm/device-policy-client.h
+  *
+  */
+
+ /**
+  *
+  *  @defgroup DPM_PASSWORD_POLICY Password policy group
+  *  @brief Password policy group provides APIs to control password policies.
+  *
+  *  @ingroup DPM_PASSWORD_POLICY
+  *  All APIs of this group are defined in dpm/password.h
+  *
+  */
+
+ /**
+  *
+  *  @defgroup DPM_SECURITY_POLICY Security policy group 
+  *  @brief Security policy group provides APIs to control security related functionality on the device
+  *  such as device encryption, and wipe.
+  *
+  *  @ingroup DPM_SECURITY_POLICY
+  *  All APIs of this group are defined in dpm/security.h
+  *  <br>
+  */
+
+ /**
+  *
+  *  @defgroup DPM_ZONE_POLICY Zone policy group
+  *  @brief Zone policy group provides APIs to control containers
+  *
+  *  @ingroup DPM_ZONE_POLICY
+  *  All APIs of this group are defined in dpm/zone.h
+  *
+  */
+
+#endif /* __DPM_DOC_H__ */
+
