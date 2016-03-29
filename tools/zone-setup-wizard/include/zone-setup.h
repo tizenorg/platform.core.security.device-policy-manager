a5fa4f9c143b25547d2a60a4bf2bc9664a5b6470
seolhee, kim <s414.kim@samsung.com>
2016-03-29 13:47:00 +0900

    Add Zone Setup-wizard
    Signed-off-by: seolhee, kim <s414.kim@samsung.com>
    
    Change-Id: If2de810f4e9d833b46d91218ee65ffd08055ca4c

---
+++ tools/zone-setup-wizard/include/zone-setup.h
@@ -0,0 +1,68 @@
+/*
+ * Tizen Zone Setup-Wizard application
+ *
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#ifndef __ZONE_SETUP_H__
+#define __ZONE_SETUP_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <app.h>
+#include <dlog.h>
+#include <app_common.h>
+#include <Elementary.h>
+#include <efl_extension.h>
+#include <dpm/zone.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "zone-setup-wizard"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.tizen.zone-setup-wizard"
+#endif
+
+#define MANIFEST_PATH "/usr/apps/org.tizen.zone-setup-wizard/data/bundle-manifest.xml" /* [TBD] : removed */
+#define SETUP_TEXT_STYLE "DEFAULT='font=Tizen:style=Regular font_size=34 color=#ffffff wrap=mixed align=center'"
+
+typedef enum {
+	WELCOME_WIN = 0,
+	SETUP_WIN,
+} view_n;
+
+typedef struct {
+	char *edj_path;
+	dpm_client_h* dpm_client;
+
+	Evas_Object *main_win;
+	Evas_Object *main_conform;
+	Evas_Object *nf;
+	Evas_Object *popup;
+
+	bool create_done;
+	view_n vnum;
+} appdata_s;
+
+void create_welcome_view(void *data);
+void create_zone_popup(void *data);
+void create_setup_zone_view(void *data);
+
+int create_zone_manifest(void);
+
+#endif /* __ZONE_SETUP_H__ */
