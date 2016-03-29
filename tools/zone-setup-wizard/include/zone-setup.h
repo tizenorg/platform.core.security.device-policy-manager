e70a86ee4347dcffb16c7cf7be88b9a6dec57ff0
seolhee, kim <s414.kim@samsung.com>
2016-03-29 13:47:00 +0900

    Add Zone Setup-wizard
    Signed-off-by: seolhee, kim <s414.kim@samsung.com>
    
    Change-Id: If2de810f4e9d833b46d91218ee65ffd08055ca4c

---
+++ tools/zone-setup-wizard/include/zone-setup.h
@@ -0,0 +1,63 @@
+/*
+ * Tizen Zone Setup-Wizard application
+ *
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#ifndef __ZONE_SETUP_H__
+#define __ZONE_SETUP_H__
+
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <app.h>
+#include <app_common.h>
+#include <bundle.h>
+#include <dlog.h>
+#include <Elementary.h>
+#include <efl_extension.h>
+#include <dpm/zone.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "zone-setup-wizard"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.tizen.zone-setup-wizard"
+#endif
+
+#define MANIFEST_PATH "/usr/apps/org.tizen.zone-setup-wizard/data/bundle-manifest.xml" /* [TBD] : removed */
+#define SETUP_TEXT_STYLE "DEFAULT='font=Tizen:style=Regular font_size=34 color=#ffffff wrap=mixed align=center'"
+
+typedef enum {
+	WELCOME_WIN = 0,
+	SETUP_WIN,
+} view_n;
+
+typedef struct {
+	char *zone_name;
+	dpm_client_h dpm_client;
+	bool create_done;
+} appdata_s;
+
+void _create_base_window(appdata_s *data);
+void _create_welcome_view(appdata_s *data);
+void _create_zone_popup(appdata_s *data);
+void _create_setup_zone_view(appdata_s *data);
+
+int _create_zone_manifest(void);
+
+#endif /* __ZONE_SETUP_H__ */
---
+++ tools/zone-setup-wizard/src/ui.c
@@ -0,0 +1,233 @@
+/*
+ * Tizen Zone Setup-Wizard application
+ *
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include "zone-setup.h"
+#include "widget.h"
+
+#define WELCOME_TEXT "Welcome<br>Keep your applications and content safe<br>with ZONE."
+#define POPUP_CONTENT "Creating Zone..."
+#define SETUP_TITLE "Tizen ZONE"
+#define SETUP_SUB_TEXT "The application shortcut will be created<br>on your personal home screen."
+
+typedef struct {
+	Evas_Object *win;
+	Evas_Object *conform;
+	Evas_Object *nf;
+	Evas_Object *popup;
+	char *edj_path;
+	view_n vnum;
+} uidata_s;
+
+uidata_s ud = {0, };
+
+static Eina_Bool __naviframe_pop_cb(void *data, Elm_Object_Item *it)
+{
+	switch (ud.vnum) {
+	case WELCOME_WIN:
+		ui_app_exit();
+		return EINA_FALSE;
+	case SETUP_WIN:
+		ud.vnum = WELCOME_WIN;
+		elm_object_signal_emit(ud.conform, "elm,state,indicator,overlap", "elm");
+		return EINA_TRUE;
+	default:
+		return EINA_FALSE;
+	}
+}
+
+static void __next_btn_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	appdata_s *ad = (appdata_s *)data;
+
+	switch (ud.vnum) {
+	case WELCOME_WIN:
+		ud.vnum = SETUP_WIN;
+		if (_create_zone_manifest() != 0)
+			ui_app_exit();
+		_create_zone_popup(ad);
+		break;
+	case SETUP_WIN:
+		ui_app_exit();
+		break;
+	default:
+		break;
+	}
+	return ;
+}
+
+static void __set_one_btn_bottom_layout(Evas_Object *layout, appdata_s *ad, const char *btn_text)
+{
+	Evas_Object *bottom_layout;
+	Evas_Object *btn;
+
+	bottom_layout = _create_layout(layout, ud.edj_path, "one_button_layout");
+
+	btn = _create_button(bottom_layout, btn_text, "bottom");
+	elm_object_part_content_set(bottom_layout, "button", btn);
+	evas_object_smart_callback_add(btn, "clicked", __next_btn_cb, ad);
+
+	elm_object_part_content_set(layout, "bottom_layout", bottom_layout);
+	return ;
+}
+
+static void __popup_del_cb(void *data, Evas *e, Evas_Object *obj, void *event_info)
+{
+	appdata_s *ad = (appdata_s *)data;
+
+	Evas_Object *timer = evas_object_data_get(obj, "timer");
+	ecore_timer_del(timer);
+
+	eext_object_event_callback_add(ud.nf, EEXT_CALLBACK_BACK, eext_naviframe_back_cb, NULL);
+	_create_setup_zone_view(ad);
+	return ;
+}
+
+static Eina_Bool __progressbar_timer_cb(void *data)
+{
+	appdata_s *ad = (appdata_s *) data;
+
+	if (ad->create_done) {
+		evas_object_data_del(ud.popup, "timer");
+		evas_object_del(ud.popup);
+
+		return ECORE_CALLBACK_CANCEL;
+	}
+
+	return ECORE_CALLBACK_RENEW;
+}
+
+void _create_base_window(appdata_s *ad)
+{
+	Evas_Object *layout;
+
+	char edj_path[PATH_MAX] = "\0";
+	char *res_path = NULL;
+
+	/* Initialize data */
+	ud.vnum = WELCOME_WIN;
+	ad->create_done = false;
+
+	/* Get EDJ path */
+	res_path = app_get_resource_path();
+	if (res_path == NULL) {
+		dlog_print(DLOG_ERROR, LOG_TAG, "failed get resource path");
+		ui_app_exit();
+	}
+	snprintf(edj_path, PATH_MAX, "%s%s", res_path, "org.tizen.zone-setup-wizard.edj");
+
+	ud.edj_path = strdup(edj_path);
+	free(res_path);
+
+	/* Create main UI widget */
+	ud.win = _create_win(PACKAGE);
+	ud.conform = _create_conformant(ud.win);
+	layout = _create_layout(ud.conform, NULL, NULL);
+	elm_object_content_set(ud.conform, layout);
+	ud.nf = elm_naviframe_add(layout);
+
+	_create_welcome_view(ad);
+
+	elm_object_part_content_set(layout, "elm.swallow.content", ud.nf);
+	eext_object_event_callback_add(ud.nf, EEXT_CALLBACK_BACK, eext_naviframe_back_cb, NULL);
+
+	evas_object_show(ud.win);
+	return ;
+}
+
+void _create_welcome_view(appdata_s *ad)
+{
+	Elm_Object_Item *nf_it;
+	Evas_Object *layout, *welcome_layout;
+	Evas_Object *text;
+	Evas_Textblock_Style *text_st;
+
+	elm_object_signal_emit(ud.conform, "elm,state,indicator,overlap", "elm");
+
+	layout = _create_layout(ud.nf, ud.edj_path, "base_layout");
+	welcome_layout = _create_layout(layout, ud.edj_path, "welcome_layout");
+
+	text_st = evas_textblock_style_new();
+	evas_textblock_style_set(text_st, SETUP_TEXT_STYLE);
+	text = _create_textblock(welcome_layout, WELCOME_TEXT, text_st);
+	elm_object_part_content_set(welcome_layout, "content_text", text);
+	evas_textblock_style_free(text_st);
+
+	elm_object_part_content_set(layout, "content_layout", welcome_layout);
+
+	__set_one_btn_bottom_layout(layout, ad, "Set up");
+
+	nf_it = elm_naviframe_item_push(ud.nf, NULL, NULL, NULL, layout, NULL);
+	elm_naviframe_item_title_enabled_set(nf_it, EINA_FALSE, EINA_TRUE);
+	elm_naviframe_item_pop_cb_set(nf_it, __naviframe_pop_cb, NULL);
+
+	return ;
+}
+
+void _create_zone_popup(appdata_s *ad)
+{
+	Evas_Object *layout;
+	Evas_Object *popup;
+	Evas_Object *progressbar, *timer;
+
+	eext_object_event_callback_del(ud.nf, EEXT_CALLBACK_BACK, eext_naviframe_back_cb);
+	popup = elm_popup_add(ud.win);
+
+	elm_popup_align_set(popup, ELM_NOTIFY_ALIGN_FILL, 1.0);
+	evas_object_size_hint_weight_set(popup, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	elm_object_part_text_set(popup, "title,text", SETUP_TITLE);
+
+	layout = _create_layout(popup, ud.edj_path, "create_popup_layout");
+	elm_object_part_text_set(layout, "text", POPUP_CONTENT);
+
+	progressbar = _create_progressbar(layout, "process_large");
+	elm_object_part_content_set(layout, "processing", progressbar);
+	elm_object_part_content_set(popup, "elm.swallow.content", layout);
+
+	evas_object_show(popup);
+	ud.popup = popup;
+
+	/* set popup timer callback*/
+	timer = ecore_timer_add(0.1, __progressbar_timer_cb, ad);
+	evas_object_data_set(popup, "timer", timer);
+	evas_object_event_callback_add(popup, EVAS_CALLBACK_DEL, __popup_del_cb, ad);
+
+	return ;
+}
+
+void _create_setup_zone_view(appdata_s *ad)
+{
+	Elm_Object_Item *nf_it;
+	Evas_Object *layout, *setup_layout;
+
+	elm_object_signal_emit(ud.conform, "elm,state,indicator,nooverlap", "elm");
+
+	layout = _create_layout(ud.nf, ud.edj_path, "base_layout");
+
+	setup_layout = _create_layout(layout, ud.edj_path, "setup_layout");
+	elm_object_part_text_set(setup_layout, "sub_title_text", SETUP_TITLE);
+	elm_object_part_text_set(setup_layout, "content_text", SETUP_SUB_TEXT);
+	elm_object_part_content_set(layout, "content_layout", setup_layout);
+
+	__set_one_btn_bottom_layout(layout, ad, "Done");
+
+	nf_it = elm_naviframe_item_push(ud.nf, NULL, NULL, NULL, layout, NULL);
+	elm_naviframe_item_title_enabled_set(nf_it, EINA_FALSE, EINA_TRUE);
+	elm_naviframe_item_pop_cb_set(nf_it, __naviframe_pop_cb, NULL);
+
+	return ;
+}
