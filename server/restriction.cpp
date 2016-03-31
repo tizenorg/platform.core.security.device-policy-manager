10fa8a976ebef8c47733293798d895d73cfecca2
seolhee, kim <s414.kim@samsung.com>
2016-03-31 15:31:57 +0900

    [WIP] Add Restriction Policy
     - add 2 dpm clipboard API in libs/server
     - add test case for addtional dpm clipboard 2 API
     - fix comment for ACR Process(enable parameter mean "set restriction")
    
    Change-Id: I22aacccc1d4bbaac91d2b4d9faf730ea76f92492
    Signed-off-by: seolhee, kim <s414.kim@samsung.com>

---
+++ server/restriction.cpp
@@ -0,0 +1,120 @@
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#include "restriction.hxx"
+#include "audit/logger.h"
+
+namespace DevicePolicyManager
+{
+
+Restriction::Restriction(PolicyControlContext& ctxt)
+	: context(ctxt)
+{
+	context.registerParametricMethod(this, (int)(Restriction::setClipboardRestriction)(bool));
+	context.registerNonparametricMethod(this, (bool)(Restriction::isClipboardRestricted));
+	context.registerParametricMethod(this, (int)(Restriction::setClipboardShareRestriction)(bool));
+	context.registerNonparametricMethod(this, (bool)(Restriction::isClipboardShareRestricted));
+	context.registerParametricMethod(this, (int)(Restriction::setSettingsChangesRestriction)(bool));
+	context.registerNonparametricMethod(this, (bool)(Restriction::isSettingsChangesRestricted));
+	context.registerParametricMethod(this, (int)(Restriction::setBackgroundDataRestriction)(bool));
+	context.registerNonparametricMethod(this, (bool)(Restriction::isBackgroundDataRestricted));
+	context.registerParametricMethod(this, (int)(Restriction::setUsbDebuggingRestriction)(bool));
+	context.registerNonparametricMethod(this, (bool)(Restriction::isUsbDebuggingRestricted));
+	context.registerParametricMethod(this, (int)(Restriction::setUsbMassStorageRestriction)(bool));
+	context.registerNonparametricMethod(this, (bool)(Restriction::isUsbMassStorageRestricted));
+	context.registerParametricMethod(this, (int)(Restriction::setFactoryResetRestriction)(bool));
+	context.registerNonparametricMethod(this, (bool)(Restriction::isFactoryResetRestricted));
+}
+
+Restriction::~Restriction()
+{
+}
+
+int Restriction::setClipboardRestriction(bool enable)
+{
+	return 0;
+}
+
+bool Restriction::isClipboardRestricted()
+{
+	return true;
+}
+
+int Restriction::setClipboardShareRestriction(bool enable)
+{
+	return 0;
+}
+
+bool Restriction::isClipboardShareRestricted()
+{
+	return true;
+}
+
+
+int Restriction::setSettingsChangesRestriction(bool enable)
+{
+	return 0;
+}
+
+bool Restriction::isSettingsChangesRestricted()
+{
+	return true;
+}
+
+int Restriction::setBackgroundDataRestriction(bool enable)
+{
+	return 0;
+}
+
+bool Restriction::isBackgroundDataRestricted()
+{
+	return true;
+}
+
+int Restriction::setUsbDebuggingRestriction(bool enable)
+{
+	return 0;
+}
+
+bool Restriction::isUsbDebuggingRestricted()
+{
+	return true;
+}
+
+int Restriction::setUsbMassStorageRestriction(bool enable)
+{
+	return 0;
+}
+
+bool Restriction::isUsbMassStorageRestricted()
+{
+	return true;
+}
+
+int Restriction::setFactoryResetRestriction(bool enable)
+{
+	return 0;
+}
+
+bool Restriction::isFactoryResetRestricted()
+{
+	return true;
+}
+
+Restriction restrictionPolicy(Server::instance());
+
+} // namespace DevicePolicyManager
+
