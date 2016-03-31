f33713fa127ae33b9063b4e0cab579081601d24c
Sungbae Yoo <sungbae.yoo@samsung.com>
2016-03-31 20:52:09 +0900

    [WIP] Add Zone::getList function
    Change-Id: Icc94d7e3e70202e4b8cb3d9c05872b6783dde7ca
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

--- server/zone.cpp
+++ server/zone.cpp
@@ -418,7 +418,20 @@ int Zone::unlockZone(const std::string& name)
 
 std::vector<std::string> Zone::getZoneList()
 {
-    return std::vector<std::string>();
+    std::vector<std::string> list;
+    try {
+        runtime::DirectoryIterator iter(runtime::Path(ZONE_MANIFEST_DIR)), end;
+
+        while (iter != end) {
+            const std::string& path = (*iter).getPath();
+            size_t name = path.rfind('/') + 1;
+            size_t ext = path.rfind(".xml");
+            list.push_back(path.substr(name, ext - name));
+            ++iter;
+        }
+    } catch (runtime::Exception& e) {}
+
+    return list;
 }
 
 int Zone::getZoneState(const std::string& name)
