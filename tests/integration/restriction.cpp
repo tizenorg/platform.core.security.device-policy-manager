df0f71266a116ab7e42fea0b4ad39794a8a1380e
seolhee, kim <s414.kim@samsung.com>
2016-03-31 15:31:57 +0900

    [WIP] Add Restriction Policy
     - add 2 dpm clipboard API
     - fix comment (enable parameter mean "set restriction")
    
    Change-Id: I22aacccc1d4bbaac91d2b4d9faf730ea76f92492
    Signed-off-by: seolhee, kim <s414.kim@samsung.com>

---
+++ tests/integration/restriction.cpp
@@ -0,0 +1,136 @@
+/*
+ *  Copyright (c) 2016 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#include "policy-client.h"
+#include "restriction.hxx"
+#include "testbench/testbench.h"
+
+#include "audit/logger.h"
+
+TESTCASE(ClipboardTest)
+{
+	DevicePolicyClient client;
+	int error = -1;
+	bool status = false;
+
+	TEST_EXPECT(0, client.connect());
+
+	DevicePolicyManager::Restriction restriction = client.createPolicyInterface<DevicePolicyManager::Restriction>();
+
+	error = restriction.setClipboardRestriction(true);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isClipboardRestricted();
+	TEST_EXPECT(true, status);
+
+	error = restriction.setClipboardRestriction(false);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isClipboardRestricted();
+	TEST_EXPECT(false, status);
+}
+
+TESTCASE(SettingsChangesTest)
+{
+	DevicePolicyClient client;
+	int error = -1;
+	bool status = false;
+
+	TEST_EXPECT(0, client.connect());
+
+	DevicePolicyManager::Restriction restriction = client.createPolicyInterface<DevicePolicyManager::Restriction>();
+
+	error = restriction.setSettingsChangesRestriction(true);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isSettingsChangesRestricted();
+	TEST_EXPECT(true, status);
+
+	error = restriction.setSettingsChangesRestriction(false);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isSettingsChangesRestricted();
+	TEST_EXPECT(false, status);
+}
+
+TESTCASE(SettingsBackgroundData)
+{
+	DevicePolicyClient client;
+	int error = -1;
+	bool status = false;
+
+	TEST_EXPECT(0, client.connect());
+
+	DevicePolicyManager::Restriction restriction = client.createPolicyInterface<DevicePolicyManager::Restriction>();
+
+	error = restriction.setBackgroundDataRestriction(true);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isBackgroundDataRestricted();
+	TEST_EXPECT(true, status);
+
+	error = restriction.setBackgroundDataRestriction(false);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isBackgroundDataRestricted();
+	TEST_EXPECT(false, status);
+}
+
+TESTCASE(UsbDebuggingTest)
+{
+	DevicePolicyClient client;
+	int error = -1;
+	bool status = false;
+
+	TEST_EXPECT(0, client.connect());
+
+	DevicePolicyManager::Restriction restriction = client.createPolicyInterface<DevicePolicyManager::Restriction>();
+
+	error = restriction.setUsbDebuggingRestriction(true);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isUsbDebuggingRestricted();
+	TEST_EXPECT(true, status);
+
+	error = restriction.setUsbDebuggingRestriction(false);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isUsbDebuggingRestricted();
+	TEST_EXPECT(false, status);
+}
+
+TESTCASE(FactoryResetTest)
+{
+	DevicePolicyClient client;
+	int error = -1;
+	bool status = false;
+
+	TEST_EXPECT(0, client.connect());
+
+	DevicePolicyManager::Restriction restriction = client.createPolicyInterface<DevicePolicyManager::Restriction>();
+
+	error = restriction.setFactoryResetRestriction(true);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isFactoryResetRestricted();
+	TEST_EXPECT(true, status);
+
+	error = restriction.setFactoryResetRestriction(false);
+	TEST_EXPECT(0, error);
+
+	status = restriction.isFactoryResetRestricted();
+	TEST_EXPECT(false, status);
+}
