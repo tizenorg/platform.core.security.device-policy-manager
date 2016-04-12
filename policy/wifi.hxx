9cd4c96ef948b7660b89f07bbda0e760722ff7a2
Jaemin Ryu <jm77.ryu@samsung.com>
2016-04-12 11:51:47 +0900

    [WIP] Add Wi-Fi Policy
    Change-Id: I147a993b78f9d1ecb316bd26a45db844c3f6473a
    Signed-off-by: Jaemin Ryu <jm77.ryu@samsung.com>

---
+++ policy/wifi.hxx
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#ifndef __WIFI_POLICY__
+#define __WIFI_POLICY__
+
+#include "data-type.h"
+#include "policy-context.hxx"
+
+namespace DevicePolicyManager {
+
+/**
+ * This class provides APIs to configure Wi-Fi related settings and manage Wi-Fi profiles.
+ */
+
+class Wifi {
+public:
+    Wifi(PolicyControlContext& ctxt);
+    ~Wifi();
+
+    int setStateChangeRestriction(bool restrict);
+    bool isStateChangeRestricted();
+
+    int setSettingChangesRestriction(bool restrict);
+    bool isSettingChangesRestricted();
+
+    int setHotspotRestriction(bool restrict);
+    bool isHotspotRestricted();
+
+    int setNetworkAccessRestriction(bool restrict);
+    bool isNetworkAccessRestricted();
+
+    int addSsidFromBlacklist(const std::string& ssid);
+    int removeSsidFromBlacklist(const std::string& ssid);
+
+private:
+    PolicyControlContext& context;
+};
+
+} // namespace DevicePolicyManager
+#endif // __WIFI_POLICY__
