e5756ec8b1d9c46c3fba1b2481ad6ad3554033d8
Jaemin Ryu <jm77.ryu@samsung.com>
2016-04-20 19:31:32 +0900

    [WIP] Add DBUS wrapper
    Change-Id: Ibc4c8d72b3de8d13c62c4f151086ea7a65d92b7d
    Signed-off-by: Jaemin Ryu <jm77.ryu@samsung.com>

---
+++ common/dbus/variant.h
@@ -0,0 +1,42 @@
+/*
+ *  Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#ifndef __RUNTIME_DBUS_VARIANT_H__
+#define __RUNTIME_DBUS_VARIANT_H__
+
+#include <string>
+#include <gio/gio.h>
+
+namespace dbus{
+
+class Variant {
+public:
+    Variant(GVariant* var);
+    Variant(Variant&& var);
+    Variant();
+    ~Variant();
+
+    Variant& operator=(GVariant* var);
+    operator bool () const;
+
+    void get(const std::string& format, ...) const;
+private:
+    GVariant* variant;
+};
+
+} // namespace dbus
+
+#endif //!__RUNTIME_DBUS_VARIANT_H__
