5fb7bec3c5bbdfc2e5ba63a9c9815cb1fb501c74
seolhee, kim <s414.kim@samsung.com>
2016-04-26 19:38:59 +0900

    Update syspopup popup text of policies
    Change-Id: Ida61f440f5b2f7dfcec426843cc04ab6799b47c6
    Signed-off-by: seolhee, kim <s414.kim@samsung.com>

--- tools/syspopup/src/popup-list.c
+++ tools/syspopup/src/popup-list.c
@@ -19,19 +19,35 @@
 
 #include "dpm-syspopup.h"
 
-static void __terminate_cb (void *user_data)
-{
-	return;
-}
+#define ARRAY_SIZE(_array_) \
+	(sizeof(__array_) / sizeof(_array_[0]))
 
-static void __end_key_cb (void *user_data)
-{
-	return;
-}
+popup_info_s popup_list[] = {
+	/* Application Policy */
+	{"package-installation-mode",   "IDS_DPM_HEADER_APPLICATION_POLICY", "IDS_DPM_BODY_PREVENT_APPLICATION_INSTALLATION_MODE"},
+	{"package-uninstallation-mode", "IDS_DPM_HEADER_APPLICATION_POLICY", "IDS_DPM_BODY_PREVENT_APPLICATION_UNINSTALLATION_MODE"},
 
-popup_info_s popup_list[DPM_SYSPOPUP_MAX] = {
-	{"restriction_camera", "IDS_DPM_POP_RESTRICTION_CAMERA_TITLE", "IDS_DPM_POP_RESTRICTION_CAMERA_CONTENT", __terminate_cb, __end_key_cb},
-	{"restriction_location", "IDS_DPM_POP_RESTRICTION_LOCATION_TITLE", "IDS_DPM_POP_RESTRICTION_CONTENT", __terminate_cb, __end_key_cb},
+	/* Password Policy */
+	{"password-expires",        "IDS_DPM_HEADER_PASSWORD_POLICY", "IDS_DPM_BODY_PASSWORD_EXPIRES"},
+	{"password-reset",          "IDS_DPM_HEADER_PASSWORD_POLICY", "IDS_DPM_BODY_PASSWORD_RESET"},
+	{"password-enforce-change", "IDS_DPM_HEADER_PASSWORD_POLICY", "IDS_DPM_BODY_PASSWORD_ENFORCE_CHANGE"},
+	{"password-delete-pattern", "IDS_DPM_HEADER_PASSWORD_POLICY", "IDS_DPM_BODY_PASSWORD_DELETE_PATTERN"},
+	{"password-set-pattern",    "IDS_DPM_HEADER_PASSWORD_POLICY", "IDS_DPM_BODY_PASSWORD_SET_PATTERN"},
+
+	/* Restriction Policy */
+	{"wifi",                    "IDS_DPM_HEADER_WIFI_POLICY", "IDS_DPM_BODY_PREVENT_WIFI"},
+	{"wifi-hotspot",            "IDS_DPM_HEADER_WIFI_POLICY", "IDS_DPM_BODY_PREVENT_HOTSPOT"},
+	{"camera",                  "IDS_DPM_HEADER_CAMERA_POLICY",   "IDS_DPM_BODY_PREVENT_CAMERA"},
+	{"microphone",              "IDS_DPM_HEADER_RECORD_POLICY",   "IDS_DPM_BODY_PREVENT_MICROPHONE"},
+	{"location",                "IDS_DPM_HEADER_LOCATION_POLICY", "IDS_DPM_BODY_PREVENT_LOCATION"},
+	{"settings-changes",        "IDS_DPM_HEADER_SETTING_POLICY",  "IDS_DPM_BODY_PREVENT_SETTINGS_CHANGES"},
+	{"bluetooth",               "IDS_DPM_HEADER_PREVENT_BLUETOOTH", "IDS_DPM_BODY_PREVENT_BLUETOOTH"},
+
+	/* Storage Policy */
+	{"storage-external",   "IDS_DPM_HEADER_STORAGE_POLICY", "IDS_DPM_BODY_PREVENT_EXTERNAL_STORAGE"},
+	{"storage-encryption", "IDS_DPM_HEADER_STORAGE_POLICY", "IDS_DPM_BODY_PREVENT_STORAGE_ENCRYPTION"},
+	{"storage-decryption", "IDS_DPM_HEADER_STORAGE_POLICY", "IDS_DPM_BODY_PREVENT_STORAGE_DECRYPTION"},
+	{"storage-wipe-data",  "IDS_DPM_HEADER_STORAGE_POLICY", "IDS_DPM_BODY_WIPE_STORAGE_DATA"},
 };
 
 popup_info_s *_get_dpm_popup_info(const char *popup_name)
@@ -43,7 +59,7 @@ popup_info_s *_get_dpm_popup_info(const char *popup_name)
 		return NULL;
 	}
 
-	for (i = 0; i < DPM_SYSPOPUP_MAX; i++) {
+	for (i = 0; i < ARRAY_SIZE(popup_list); i++) {
 		if (!strcmp(popup_name, popup_list[i].name))
 			return &popup_list[i];
 	}
