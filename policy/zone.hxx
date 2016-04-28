3c44c8b2ae6c7b318a93ac82c44c4029373a2a46
Sungbae Yoo <sungbae.yoo@samsung.com>
2016-04-28 19:49:58 +0900

    [WIP] Package and application APIs in zone policy
    Change-Id: Ia215b0ef9590658e8181f0eb06e42fe14938b0e2
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>

--- policy/zone.hxx
+++ policy/zone.hxx
@@ -37,8 +37,35 @@ public:
     int unlockZone(const std::string& name);
 
     std::vector<std::string> getZoneList(void);
-
     int getZoneState(const std::string& name);
+
+    //package information
+    std::vector<std::string> getPackageList(const std::string& zone);
+    std::string getPackageIcon(const std::string& zone, const std::string& pkgid);
+    std::string getPackageLabel(const std::string& zone, const std::string& pkgid);
+    std::string getPackageVersion(const std::string& zone, const std::string& pkgid);
+    bool isSystemPackage(const std::string& zone, const std::string& pkgid);
+    bool isRemovablePackage(const std::string& zone, const std::string& pkgid);
+    bool isPreloadPackage(const std::string& zone, const std::string& pkgid);
+
+    //application information
+    std::vector<std::string> getUIAppList(const std::string& zone, const std::string &pkgid);
+    std::string getAppIcon(const std::string& zone, const std::string& appid);
+    std::string getAppLabel(const std::string& zone, const std::string& appid);
+    bool isNoDisplayedApp(const std::string& zone, const std::string& appid);
+    bool isTaskmanagedApp(const std::string& zone, const std::string& appid);
+
+    //package manager request
+    int installPackage(const std::string& zone, const std::string& pkgpath);
+    int uninstallPackage(const std::string& zone, const std::string& pkgid);
+
+    //app control for homescreen
+    int launchApp(const std::string& zone, const std::string& appid);
+
+    //app control for task manager
+    int resumeApp(const std::string& zone, const std::string& appid);
+    int terminateApp(const std::string& zone, const std::string& appid);
+    bool isAppRunning(const std::string& zone, const std::string& appid);
 
 private:
     PolicyControlContext& context;
