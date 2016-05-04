4beac1aa0ba528b4aeb9df6f326456c39361f062
yeji01kim <yeji01.kim@samsung.com>
2016-05-04 15:24:26 +0900

    Add container encryption
    Change-Id: I216d7911258d884f73cf32a3bc945028bc603361
    Signed-off-by: yeji01kim <yeji01.kim@samsung.com>

---
+++ zone/volume/key-manager.h
@@ -0,0 +1,35 @@
+/*
+ *  Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ */
+
+#ifndef __ZONE_KEY_MANAGER_H__
+#define __ZONE_KEY_MANAGER_H__
+
+class KeyManager {
+public:
+    KeyManager() = delete;
+    KeyManager(const KeyManager&) = delete;
+    KeyManager(KeyManager&&) = delete;
+
+    KeyManager& operator=(const KeyManager&) = delete;
+    KeyManager& operator=(KeyManager&&) = delete;
+
+    static bool isKeyExist(const std::string& keyName);
+    static void addKey(const std::string& keyName, const std::string& data);
+    static std::string getKey(const std::string& keyName);
+    static void removeKey(const std::string& keyName);
+};
+
+#endif //!__ZONE_KEY_MANAGER_H__*/
