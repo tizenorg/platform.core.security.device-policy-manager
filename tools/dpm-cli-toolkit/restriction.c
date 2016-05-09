94d45ef478c9420a83fafd23a317c47d0384d74b
Sungjun, Lee <sjun221.lee@samsung.com>
2016-05-09 13:51:10 +0900

    ADD DPM command line interface toolkit
    Change-Id: Ibe57fd06c2255fbb35c83424fdf39a74bdbbe3d1
    Signed-off-by: Sungjun, Lee <sjun221.lee@samsung.com>

---
+++ tools/dpm-cli-toolkit/restriction.c
@@ -0,0 +1,443 @@
+#include "dpm-cli-toolkit.h"
+#include <dpm/restriction.h>
+
+int set_camera_state_handler(int state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed to create device policy context handle\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed to create camera policy handle\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_set_camera_state(policy, state) == 0) {
+        dpm_context_release_restriction_policy(context, policy);
+        dpm_context_destroy(context);
+        return 0;
+    }
+
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+    return -1;
+}
+
+int get_camera_state_handler(int *state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed to create device policy context handle\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed to create camera policy handle");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_get_camera_state(policy, state) == 0) {
+        dpm_context_release_restriction_policy(context, policy);
+        dpm_context_destroy(context);
+        return 0;
+    }
+
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+    return -1;
+}
+
+int set_microphone_state_handler(int state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed to create device policy context handle\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed to create microphone policy handle\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_set_microphone_state(policy, state) == 0) {
+        dpm_context_release_restriction_policy(context, policy);
+        dpm_context_destroy(context);
+        return 0;
+    }
+
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+    return -1;
+}
+
+int get_microphone_state_handler(int *state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed to create device policy context handle\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed to create microphone policy handle\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_get_microphone_state(policy, state) == 0) {
+        dpm_context_release_restriction_policy(context, policy);
+        dpm_context_destroy(context);
+        return 0;
+    }
+
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+    return -1;
+}
+
+int set_location_state_handler(int state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed to create dpm context\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed to create client handle\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_set_location_state(policy, state) != 0) {
+        printf("Failed to set location state\n");
+        dpm_context_release_restriction_policy(context, policy);
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+    return 0;
+}
+
+int get_location_state_handler(int *state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed to create dpm context\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed to create client handle\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    dpm_restriction_get_location_state(policy, state);
+    if (state < 0) {
+        printf("Failed to get location state\n");
+        dpm_context_release_restriction_policy(context, policy);
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+
+    return 0;
+}
+
+int set_clipboard_state_handler(int state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+    int ret = 0;
+    int get_value = 0;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed in dpm_context_create()\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed in dpm_context_acquire_restriction_policy()\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_set_clipboard_state(policy, state) != 0) {
+        printf("Failed in dpm_restriction_set_clipboard_state()\n");
+        ret = -1;
+        goto out;
+    }
+
+    if (dpm_restriction_get_clipboard_state(policy, &get_value) < 0) {
+        printf("Failed in dpm_restriction_get_clipboard_state()\n");
+        ret = -1;
+        goto out;
+    }
+
+    if (state != get_value) {
+        printf("Failed to set value of restriction\n");
+        ret = -1;
+        goto out;
+    }
+out:
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+
+    if (ret == -1)
+        return ret;
+    else
+        return 0;
+}
+
+int get_clipboard_state_handler(int *state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+    int ret = 0;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed in dpm_context_create()\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed in dpm_context_acquire_restriction_policy()\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_get_clipboard_state(policy, state) < 0) {
+        printf("Failed in dpm_restriction_get_clipboard_state()\n");
+        ret = -1;
+        goto out;
+    }
+
+    if (*state == 1 || *state == 0)
+        ret = 0;
+    else
+        ret = -1;
+
+out:
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+
+    if (ret == -1)
+        return ret;
+    else
+        return 0;
+}
+
+int set_settings_changes_restriction_handler(int state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+    int ret = 0;
+    int get_value = 0;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed in dpm_context_create()\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed in dpm_context_acquire_restriction_policy()\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_set_settings_changes_state(policy, state) != 0) {
+        printf("Failed in dpm_restriction_set_settings_changes_state()\n");
+        ret = -1;
+        goto out;
+    }
+
+    if (dpm_restriction_get_settings_changes_state(policy, &get_value) < 0) {
+        printf("Failed in dpm_restriction_get_settings_changes_state()\n");
+        ret = -1;
+        goto out;
+    }
+
+    if (state != get_value) {
+        printf("Failed to set value of restriction\n");
+        ret = -1;
+        goto out;
+    }
+
+out:
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+
+    if (ret == -1)
+        return ret;
+    else
+        return 0;
+}
+
+int get_settings_changes_restriction_handler(int *state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+    int ret = 0;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed in dpm_context_create()");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed in dpm_context_acquire_restriction_policy()\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_get_settings_changes_state(policy, state) < 0) {
+        printf("Failed in dpm_restriction_get_settings_changes_state()\n");
+        ret = -1;
+        goto out;
+    }
+
+    if (*state == 1 || *state == 0)
+        ret = 0;
+    else
+        ret = -1;
+
+out:
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+
+    if (ret == -1)
+        return ret;
+    else
+        return 0;
+}
+
+int set_usb_debugging_state_handler(int state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+    int ret = 0;
+    int get_value = 0;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed in dpm_context_create()\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed in dpm_context_acquire_restriction_policy()\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_set_usb_debugging_state(policy, state) != 0) {
+        printf("Failed in dpm_restriction_set_usb_debugging_state()\n");
+        ret = -1;
+        goto out;
+    }
+
+    if (dpm_restriction_get_usb_debugging_state(policy, &get_value) < 0) {
+        printf("Failed in dpm_restriction_get_usb_debugging_state()\n");
+        ret = -1;
+        goto out;
+    }
+
+    if (state != get_value) {
+        printf("Failed to set value of restriction\n");
+        ret = -1;
+        goto out;
+    }
+
+out:
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+
+    if (ret == -1)
+        return ret;
+    else
+        return 0;
+}
+
+int get_usb_debugging_state_handler(int *state)
+{
+    dpm_context_h context = NULL;
+    dpm_restriction_policy_h policy = NULL;
+    int ret = 0;
+
+    context = dpm_context_create();
+    if (context == NULL) {
+        printf("Failed in dpm_context_create()\n");
+        return -1;
+    }
+
+    policy = dpm_context_acquire_restriction_policy(context);
+    if (policy == NULL) {
+        printf("Failed in dpm_context_acquire_restriction_policy()\n");
+        dpm_context_destroy(context);
+        return -1;
+    }
+
+    if (dpm_restriction_get_usb_debugging_state(policy, state) < 0) {
+        printf("Failed in dpm_restriction_get_usb_debugging_state()\n");
+        ret = -1;
+        goto out;
+    }
+
+    if (*state == 1 || *state == 0)
+        ret = 0;
+    else
+        ret = -1;
+
+out:
+    dpm_context_release_restriction_policy(context, policy);
+    dpm_context_destroy(context);
+
+    if (ret == -1)
+        return ret;
+    else
+        return 0;
+}
