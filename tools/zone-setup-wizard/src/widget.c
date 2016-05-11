faab8ba1107f69313975c4bb08fd98a48f01ef5d
seolhee, kim <s414.kim@samsung.com>
2016-05-11 17:07:08 +0900

    [WIP] Update zone setup-wizard UI
    Change-Id: Ie60b56b3bb30aa0cf791cee1b45d707a99a021d4
    Signed-off-by: seolhee, kim <s414.kim@samsung.com>

--- tools/zone-setup-wizard/src/widget.c
+++ tools/zone-setup-wizard/src/widget.c
@@ -30,10 +30,10 @@ Evas_Object *_create_win(const char *package)
 	elm_win_conformant_set(win, EINA_TRUE);
 	elm_win_autodel_set(win, EINA_TRUE);
 
-	if (elm_win_wm_rotation_supported_get(win)) {
+/*	if (elm_win_wm_rotation_supported_get(win)) {
 		int rots[4] = { 0, 90, 180, 270 };
 		elm_win_wm_rotation_available_rotations_set(win, (const int *)(&rots), 4);
-	}
+	}*/
 
 	evas_object_smart_callback_add(win, "delete,request", __win_delete_request_cb, NULL);
 	elm_win_indicator_mode_set(win, ELM_WIN_INDICATOR_SHOW);
@@ -101,11 +101,13 @@ Evas_Object *_create_progressbar(Evas_Object *parent, const char *style)
 {
 	Evas_Object *progressbar = elm_progressbar_add(parent);
 
-	elm_object_style_set(progressbar, style);
+	if (style != NULL)
+		elm_object_style_set(progressbar, style);
+
 	evas_object_size_hint_align_set(progressbar, EVAS_HINT_FILL, 0.5);
 	evas_object_size_hint_weight_set(progressbar, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
 	elm_progressbar_pulse(progressbar, EINA_TRUE);
-	elm_progressbar_value_set(progressbar, 0.0);
+	elm_progressbar_pulse_set(progressbar, EINA_TRUE);
 	evas_object_show(progressbar);
 
 	return progressbar;
