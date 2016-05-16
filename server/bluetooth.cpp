dfc989b771777e412d742990f477512844a566f5
Seok Hong <seok85.hong@samsung.com>
2016-05-16 14:52:10 +0900

    Add Bluetooth adapter callback for state handling
    Change-Id: Ie45d682a2a512ff1e664fe72c6d4d8c25da8391a
    Signed-off-by: Seok Hong <seok85.hong@samsung.com>

--- server/bluetooth.cpp
+++ server/bluetooth.cpp
@@ -23,6 +23,50 @@
 #include "policy-helper.h"
 #include "audit/logger.h"
 
+namespace {
+
+/**
+ * @brief The context hold bluetooth policy class ans policy context for callback
+ */
+struct BluetoothPolicyHelperContext {
+    BluetoothPolicyHelperContext(DevicePolicyManager::BluetoothPolicy* bt_policy,
+                                 PolicyControlContext* bt_ctxt) {
+        _bt_policy = bt_policy;
+        _bt_ctxt = bt_ctxt;
+    }
+    DevicePolicyManager::BluetoothPolicy* _bt_policy;
+    PolicyControlContext* _bt_ctxt;
+};
+
+/**
+ * @brief The callback function to invoke when BT adaper's stae were changed.
+ */
+void bluetoothAdapterStateChangedCb(int result, bt_adapter_state_e state, void *user_data)
+{
+    if (user_data == nullptr)
+        throw runtime::Exception("Invalid argument");
+
+    if (state == BT_ADAPTER_ENABLED) {
+        BluetoothPolicyHelperContext *bluetooth = (BluetoothPolicyHelperContext*)user_data;
+        DevicePolicyManager::BluetoothPolicy &bt_policy = dynamic_cast<DevicePolicyManager::BluetoothPolicy &>(*(bluetooth->_bt_policy));
+        PolicyControlContext &bt_ctxt = dynamic_cast<PolicyControlContext &>(*(bluetooth->_bt_ctxt));
+
+        // NOTE: "re-enforce-policy" : we can notify to admin client with this notification,
+        //                             when we should be re-enforced again for the policy that we were failed to set into Bluetooth
+        int ret = BLUETOOTH_DPM_RESULT_SUCCESS;
+        ret = bt_policy.setDeviceRestriction(IsPolicyEnabled(bt_ctxt, "bluetooth-device-restriction"));
+        if (ret != BLUETOOTH_DPM_RESULT_SUCCESS) {
+            (bt_ctxt).notify("re-enforce-policy", "bluetooth-device-restriction");
+        }
+        ret = bt_policy.setUuidRestriction(IsPolicyEnabled(bt_ctxt, "bluetooth-uuid-restriction"));
+        if (ret != BLUETOOTH_DPM_RESULT_SUCCESS) {
+            (bt_ctxt).notify("re-enforce-policy", "bluetooth-uuid-restriction");
+        }
+    }
+}
+
+} // namespace
+
 namespace DevicePolicyManager {
 
 BluetoothPolicy::BluetoothPolicy(PolicyControlContext& ctxt) :
@@ -40,10 +84,22 @@ BluetoothPolicy::BluetoothPolicy(PolicyControlContext& ctxt) :
     ctxt.createNotification("bluetooth");
     ctxt.createNotification("bluetooth-uuid-restriction");
     ctxt.createNotification("bluetooth-device-restriction");
+
+    // Register
+    int ret = bt_initialize();
+    if (ret != BT_ERROR_NONE) {
+        throw runtime::Exception("failed to initialize the Bluetooth API");
+    }
+
+    ret = bt_adapter_set_state_changed_cb(::bluetoothAdapterStateChangedCb, new BluetoothPolicyHelperContext(this, &ctxt));
+    if (ret != BT_ERROR_NONE) {
+        throw runtime::Exception("failed to register a callback function to be invoked when the Bluetooth adapter state changes");
+    }
 }
 
 BluetoothPolicy::~BluetoothPolicy()
 {
+    bt_deinitialize();
 }
 
 int BluetoothPolicy::addDeviceToBlacklist(const std::string& mac)
