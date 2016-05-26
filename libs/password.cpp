97c25195cf86f919d56485dfa94fa104cd591246
Sungjun, Lee <sjun221.lee@samsung.com>
2016-05-26 18:56:20 +0900

    Add Forbidden String Password Policy
    Change-Id: I8088b47d9e7641bd1b1f622100293d4e92f53e39
    Signed-off-by: Sungjun, Lee <sjun221.lee@samsung.com>

--- libs/password.cpp
+++ libs/password.cpp
@@ -255,10 +255,11 @@ int PasswordPolicy::setForbiddenStrings(const std::vector<std::string>& forbidde
 
 std::vector<std::string> PasswordPolicy::getForbiddenStrings()
 {
+    std::vector<std::string> error;
     try {
         return __context->methodCall<std::vector<std::string>>("PasswordPolicy::getForbiddenStrings");
     } catch (runtime::Exception& e) {
-        return std::vector<std::string>();
+        return error;
     }
 }
 } /* namespace DevicePolicyManager */
