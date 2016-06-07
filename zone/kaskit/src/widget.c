b627884f4f7ef017cdd0c421a874f6687957d6cb
Sungbae Yoo <sungbae.yoo@samsung.com>
2016-06-07 21:17:49 +0900

    Kaskit: Add to show application icons in the zones for launch
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>
    Change-Id: Iab1dfa59d34e08364e23f23f862f915f215ca5f3

--- zone/kaskit/src/widget.c
+++ zone/kaskit/src/widget.c
@@ -19,20 +19,42 @@
 
 #include "widget.h"
 
+typedef struct {
+	_toast_cb callback;
+	void* data;
+	Evas_Object *win;
+} _toast_cb_s;
+
 static void __win_delete_request_cb(void *data , Evas_Object *obj , void *event_info)
 {
 	ui_app_exit();
 }
 
+static void __toast_timeout_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	_toast_cb_s* cb_data = (_toast_cb_s*)data;
+
+        evas_object_del(cb_data->win);
+	(cb_data->callback)(cb_data->data, obj);
+	free(cb_data);
+
+        return;
+}
+
 Evas_Object *_create_win(const char *package)
 {
-	Evas_Object *win = elm_win_add(NULL, "kaskit", ELM_WIN_BASIC);
+	Evas_Object *win;
 
+	elm_app_base_scale_set(1.8);
+
+	win  = elm_win_add(NULL, package, ELM_WIN_BASIC);
 	elm_win_conformant_set(win, EINA_TRUE);
 	elm_win_autodel_set(win, EINA_TRUE);
 	elm_win_alpha_set(win, EINA_TRUE);
 
 	evas_object_smart_callback_add(win, "delete,request", __win_delete_request_cb, NULL);
+
+	eext_object_event_callback_add(win, EEXT_CALLBACK_BACK, __win_delete_request_cb, NULL);
 
 	return win;
 }
@@ -63,4 +85,26 @@ Evas_Object *_create_layout(Evas_Object *parent, char *file, const char *group)
 	evas_object_show(layout);
 
 	return layout;
+}
+
+void _create_toast(char* text, _toast_cb callback, void* user_data)
+{
+	Evas_Object* win, *conform, *popup;
+
+	win = _create_win("toast");
+	conform = _create_conformant(win);
+	popup = elm_popup_add(conform);
+	elm_object_style_set(popup, "toast");
+	elm_object_text_set(popup, text);
+
+	_toast_cb_s* cb_data = malloc(sizeof(_toast_cb_s));
+	cb_data->callback = callback;
+	cb_data->data = user_data;
+	cb_data->win = win;
+
+	elm_popup_timeout_set(popup, 3.0);
+	evas_object_smart_callback_add(popup, "timeout", __toast_timeout_cb, cb_data);
+
+	evas_object_show(popup);
+	evas_object_show(win);
 }
