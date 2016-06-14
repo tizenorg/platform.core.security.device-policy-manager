e8a4c15d54833d42c08e74746e21cab28f76b1df
yeji01kim <yeji01.kim@samsung.com>
2016-06-14 18:26:54 +0900

    [WIP] Add dpm encrypt ug app
    Change-Id: Icc0be415ea385630977cad28f5cf1e92e253c474
    Signed-off-by: yeji01kim <yeji01.kim@samsung.com>

--- tools/ode/include/ode-app.h
+++ tools/ode/include/ode-app.h
@@ -29,7 +29,18 @@
 #include <dlog.h>
 #include <efl_extension.h>
 #include <Elementary.h>
-#include <ui-gadget.h>
+#include <system_settings.h>
+#include <limits.h>
+#include <device/battery.h>
+#include <device/callback.h>
+#include <runtime_info.h>
+#include <vconf.h>
+#include <gio/gio.h>
+#include <app_control.h>
+#include <aul.h>
+#include <bundle.h>
+
+#include "ode-app-widget.h"
 
 #ifdef  LOG_TAG
 #undef  LOG_TAG
@@ -40,7 +51,135 @@
 #define PACKAGE "org.tizen.ode"
 #endif
 
-#define ENCRYPTION_UG "lockscreen-options"
-#define DECRYPTION_UG "setting-privacy-efl"
+typedef struct dpm_encryption_info {
+	int battery_status;
+	int charger_status;
+	int locktype_status;
 
-#endif /* __ODE_H__ */
+	int sdcard_status;
+	int sdcard_pw_status;
+
+	Evas_Object *next_button;
+} dpm_encryption_info_s;
+
+typedef enum {
+	ENCRYPT_DEVICE,
+	DECRYPT_DEVICE,
+	ENCRYPT_SD_CARD,
+	DECRYPT_SD_CARD,
+} dpm_encryption_viewtype;
+
+typedef struct odedata {
+	Evas_Object *win;
+	Evas_Object *conform;
+	Evas_Object *nf;
+	Evas_Object *base;
+	Evas_Object *navi_bar;
+	Evas_Object *navi_item;
+	Evas_Object *dpm_encryption_layout;
+	dpm_encryption_info_s device_info;
+
+	char *edj_path;
+	int view_type;
+} appdata_s;
+
+appdata_s global_ad;
+
+#define MSG_TEXT_STYLE "DEFAULT='font=Tizen:style=Regular font_size=30 color=#000000 wrap=mixed align=left valign=center'"
+#define NEEDED_BATTERY_PERCENT 80
+
+/* for Encrypt device */
+#define IDS_ST_HEADER_ENCRYPTION "Encryption"
+#define IDS_ST_MBODY_ENCRYPT_DEVICE "Encrypt device"
+#define IDS_ST_MBODY_DECRYPT_DEVICE "Decrypt device"
+#define IDS_ST_SBODY_DEVICE_NOT_ENCRYPTED_M_STATUS "Device not encrypted."
+#define IDS_ST_SBODY_DEVICE_ENCRYPTED_M_STATUS "Device encrypted."
+
+#define IDS_ST_MBODY_ENCRYPT_SD_CARD_ABB "Encrypt SD card"
+#define IDS_ST_MBODY_DECRYPT_SD_CARD "Decrypt SD card"
+#define IDS_ST_SBODY_SD_CARD_NOT_INSERTED_M_STATUS "SD card not inserted"
+#define IDS_ST_SBODY_SD_CARD_NOT_ENCRYPTED_M_STATUS_SBODY "SD card not encrypted"
+#define IDS_ST_SBODY_SD_CARD_ENCRYPTED_M_STATUS "SD card encrypted"
+#define IDS_ST_SBODY_ENCRYPTING_ING_M_STATUS "Encrypting..."
+#define IDS_ST_SBODY_DECRYPTING_ING_M_STATUS "Decrypting..."
+#define IDS_ST_SBODY_FAILED_TO_ENCRYPT_SD_CARD_TAP_HERE_TO_TRY_AGAIN "Failed to encrypt SD card. Tap here to try again."
+#define IDS_ST_SBODY_FAILED_TO_DECRYPT_SD_CARD_TAP_HERE_TO_TRY_AGAIN "Failed to decrypt SD card. Tap here to try again."
+#define IDS_ST_SBODY_TAP_HERE_TO_ENTER_PW_FOR_SD_CARD_ABB "Tap here to enter PW for SD card."
+
+#define IDS_ST_HEADER_ENCRYPT_DEVICE "Encrypt device"
+#define IDS_ST_BODY_ENCRYPTING_MSG_1 "Encrypting your device will secure the apps, data, and other content on your device. To decrypt your device once it has been encrypted, you will need to unlock the screen whenever the device is powered on."
+#define IDS_ST_BODY_ENCRYPTING_MSG_2 "The encryption process may take a long time<br>depending on the amount of data that is encrypted. Interrupting this process may cause data loss, so make sure you have time for it to finish before you start."
+#define IDS_ST_BODY_TO_ENCRYPTION_YOUR_DEVICE_C_ABB "To encrypt your device:"
+#define IDS_ST_BODY_CHARGE_THE_BATTERY_TO_AT_LEAST_PDP "Charge the battery to at least 80%."
+#define IDS_ST_BODY_CONNECT_THE_DEVICE_TO_A_CHARGER "Connect the device to a charger."
+#define IDS_ST_BODY_SET_THE_SCREEN_LOCK_TYPE_AS_PATTERN_PIN_OR_PASSWORD "Set the screen lock type as pattern, PIN, or password."
+#define IDS_ST_BUTTON_SCREEN_LOCK_TYPE_ABB "Screen lock type"
+#define IDS_ST_BUTTON_NEXT "Next"
+
+/* for Decrypt device */
+#define IDS_ST_HEADER_DECRYPT_DEVICE "Decrypt device"
+#define IDS_ST_BODY_DECRYPTING_MSG_1 "Decrypting your device will decrypt the apps, data, and other content on your device."
+#define IDS_ST_BODY_DECRYPTING_MSG_2 "This may take a long time depending on the amount of data. Interrupting this process may cause data loss, so make sure you have time for it to finish before you start."
+#define IDS_ST_HEADER_TO_DECRYPT_YOUR_DEVICE_C_ABB "To decrypt your device:"
+
+/* for Encrypt SD card */
+#define IDS_ST_HEADER_ENCRYPT_SD_CARD "Encrypt SD card"
+#define IDS_ST_BODY_SD_CARD_ENCRYPTING_MSG_1 "Encrypting your SD card will protect its data if it's lost or stolen. SD cards encrypted on this device can't be used on any other device."
+#define IDS_ST_BODY_SD_CARD_ENCRYPTING_MSG_2 "Once the SD card is encrypted, a password is required to use it. The same password must be used for all encrypted SD cards. If you enter an incorrect password 15 times in a row, you will no longer be able to use any of the SD cards encrypted by this device."
+#define IDS_ST_BODY_SD_CARD_ENCRYPTING_MSG_3 "The encryption process may take a long time depending on the amount of data that is encrypted. Interrupting this process may cause data loss, so make sure you have time for it to finish before you start."
+#define IDS_ST_BODY_TO_ENCRYPT_YOUR_SD_CARD_C "To encrypt your SD card :"
+#define IDS_ST_BODY_INSERT_THE_SD_CARD_ABB "Insert the SD card to encrypt."
+#define IDS_ST_BODY_CREATE_AN_SD_CARD_PASSWORD "Create an SD card password."
+#define IDS_ST_BUTTON_CREATE_PASSWORD "Create password"
+#define IDS_ST_BUTTON_CHANGE_PW_ABB "Change PW"
+#define IDS_ST_BUTTON_DELETE_PW_ABB "Delete PW"
+
+/* for Decrypt SD card */
+#define IDS_ST_HEADER_DECRYPT_SD_CARD "Decrypt SD card"
+#define IDS_ST_BODY_SD_CARD_DECRYPTION_MSG "The decryption process may take a long time depending on the amount of data that is decrypted. Interrupting this process may cause data loss, so make sure you have time for it to finish before you start."
+#define IDS_ST_BUTTON_CHANGE_PASSWORD_ABB2 "Change password"
+
+/* for Encrypt Device Confirm */
+#define IDS_ST_HEADER_CONFIRM_ENCRYPTION "Confirm encryption"
+#define IDS_ST_BODY_CONFIRM_ENCRYPT_DEVICE_MSG_1 "Tap Encrypt device to start the encryption process."
+#define IDS_ST_BODY_CONFIRM_ENCRYPT_DEVICE_MSG_2 "The encryption can't be cancelled and data will be lost if the process is interrupted. During the encryption process, your device can't be used. After the encryption, your device will restart."
+#define IDS_ST_HEADER_ENCRYPTION_OPTIONS_ABB "Encryption options"
+#define IDS_ST_MBODY_FAST_ENCRYPTION "Fast Encryption"
+#define IDS_ST_SBODY_ENCRYPT_USED_DEVICE_STORAGE_ONLY "Encrypt used device storage only."
+#define IDS_ST_BUTTON_ENCRYPT_DEVICE "Encrypt device"
+
+/* for Decrypt Device Confirm */
+#define IDS_ST_HEADER_CONFIRM_DECRYPTION "Confirm decryption"
+#define IDS_ST_BODY_CONFIRM_DECRYPT_DEVICE_MSG_1 "Tap Decrypt device to start the decryption process."
+#define IDS_ST_BODY_CONFIRM_DECRYPT_DEVICE_MSG_2 "The decryption can't be cancelled and data will be lost if the process is interrupted. During the decryption process, your device can't be used. After the decryption, your device will restart."
+#define IDS_ST_BUTTON_DECRYPT_DEVICE "Decrypt device"
+
+/* for Encrypt SD card Confirm */
+#define IDS_ST_BODY_CONFIRM_ENCRYPT_SD_CARD_MSG_1 "Tap Encrypt SD card to start the encryption process. All open files on SD card will be closed."
+#define IDS_ST_BODY_CONFIRM_ENCRYPT_SD_CARD_MSG_2 "The encryption can't be cancelled and data will be lost if the process is interrupted. During the encryption process, your SD card can't be used. After the encryption, your SD card will be mounted again."
+#define IDS_ST_MBODY_ENCRYPT_NEW_FILES "Encrypt all files"
+#define IDS_ST_BODY_ENCRYPT_NEW_FILES_MSG "Encrypt all files saved on the SD card. New files will also be encrypted."
+#define IDS_ST_MBODY_ENCRYPT_ALL_FILES "Encrypt new files"
+#define IDS_ST_BODY_ENCRYPT_ALL_FILES_MSG "Encrypt all new files created using this device. Existing files will not be encrypted."
+#define IDS_ST_BUTTON_ENCRYPT_SD_CARD_ABB "Encrypt SD card"
+
+/* for Decrypt SD Card Confirm */
+#define IDS_ST_BODY_CONFIRM_DECRYPT_SD_CARD_MSG_1 "Tap Decrypt SD card to start the decryption process. All open files on SD card will be closed."
+#define IDS_ST_BODY_CONFIRM_DECRYPT_SD_CARD_MSG_2 "The decryption can't be cancelled and data will be lost if the process is interrupted. During the decryption process, your SD card can't be used. After the decryption, your SD card will be mounted again."
+#define IDS_ST_BUTTON_DECRYPT_SD_CARD_ABB3 "Decrypt SD card"
+
+void dpm_encryption_create_view(appdata_s * ad);
+void dpm_decryption_create_view(appdata_s * ad);
+void dpm_encryption_sd_card_create_view(appdata_s * ad);
+void dpm_decryption_sd_card_create_view(appdata_s * ad);
+
+void dpm_encryption_device_confirm_create_view(appdata_s * ad);
+void dpm_decryption_device_confirm_create_view(appdata_s * ad);
+void dpm_encryption_sd_card_confirm_create_view(appdata_s * ad);
+void dpm_decryption_sd_card_confirm_create_view(appdata_s * ad);
+
+void locktype_status_changed_cb(keynode_t * node, void *data);
+void sdcard_status_changed_cb(keynode_t * node, void *data);
+void battery_changed_cb(device_callback_e type, void *value, void *user_data);
+
+#endif							/* __ODE_H__ */
--- tools/ode/src/ode-app.c
+++ tools/ode/src/ode-app.c
@@ -21,153 +21,222 @@
 
 #include "ode-app.h"
 
-static void __win_delete_request_cb(void *data, Evas_Object *obj, void *event_info)
+static Evas_Object* create_mainview(appdata_s* ad)
+{
+	Evas_Object* base_layout = NULL;
+	Evas_Object* navi_bar = NULL;
+
+	base_layout = dpm_encryption_create_layout(ad->conform, NULL, NULL);
+
+	navi_bar = dpm_encryption_create_navigation(base_layout);
+	elm_naviframe_prev_btn_auto_pushed_set(navi_bar, EINA_TRUE);
+	ad->navi_bar = navi_bar;
+
+	switch (ad->view_type) {
+	case ENCRYPT_DEVICE:
+		dpm_encryption_create_view(ad);
+		break;
+	case DECRYPT_DEVICE:
+		dpm_decryption_create_view(ad);
+		break;
+	case ENCRYPT_SD_CARD:
+		dpm_encryption_sd_card_create_view(ad);
+		break;
+	case DECRYPT_SD_CARD:
+		dpm_decryption_sd_card_create_view(ad);
+		break;
+	}
+	return base_layout;
+}
+
+static void win_delete_request_cb(void* data, Evas_Object* obj, void* event_info)
 {
 	ui_app_exit();
 }
 
-static void __ug_layout_cb(ui_gadget_h ug, enum ug_mode mode, void *priv)
+static Eina_Bool _home_key_cb(void *data, int type, void *event)
 {
-	Evas_Object *ug_layout;
-	ug_layout = (Evas_Object *) ug_get_layout(ug);
-	if (ug_layout == NULL)
-		return;
-
-	switch (mode) {
-	case UG_MODE_FULLVIEW:
-		evas_object_size_hint_weight_set(ug_layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-		evas_object_show(ug_layout);
-		break;
-	default:
-		break;
-	}
-
-	return;
+	Evas_Event_Key_Down *ev = event;
+	if (!strcmp(ev->keyname, "XF86Home"))
+		ui_app_exit();
+	return EINA_TRUE;
 }
 
-static void __ug_destroy_cb(ui_gadget_h ug, void *priv)
+static int create_base_gui(appdata_s* ad)
 {
-	ug_destroy(ug);
-	ui_app_exit();
-}
+	dlog_print(DLOG_DEBUG, LOG_TAG, "### create base gui start  ###");
+	char* res_path = NULL;
+	char edj_path[PATH_MAX] = "\0";
 
-static int __create_ug(const char *ug_name)
-{
-	struct ug_cbs cbs;
+	/* get EDJ path */
+	res_path = app_get_resource_path();
+	if (res_path == NULL) {
+		dlog_print(DLOG_ERROR, LOG_TAG, "failed get resource path");
+		ui_app_exit();
+		return 1;
+	}
+	snprintf(edj_path, PATH_MAX, "%s%s", res_path, "org.tizen.ode.edj");
+	ad->edj_path = strdup(edj_path);
+	free(res_path);
 
-	cbs.layout_cb = __ug_layout_cb;
-	cbs.destroy_cb = __ug_destroy_cb;
-	cbs.priv = NULL;
+	/* Window */
+	ad->win = elm_win_util_standard_add(PACKAGE, PACKAGE);
+	elm_win_conformant_set(ad->win, EINA_TRUE);
+	elm_win_autodel_set(ad->win, EINA_TRUE);
 
-	if (ug_name == NULL) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "ug_name is NULL");
-		return -1;
+	if (elm_win_wm_rotation_supported_get(ad->win)) {
+		int rots[4] = { 0, 90, 180, 270 };
+		elm_win_wm_rotation_available_rotations_set(ad->win, (const int *)(&rots), 4);
 	}
 
-	if (ug_create(NULL, ug_name, UG_MODE_FULLVIEW, NULL, &cbs) == NULL) {
-		dlog_print(DLOG_ERROR, LOG_TAG, "failed to create ug");
-		return -1;
-	}
+	evas_object_smart_callback_add(ad->win, "delete,request", win_delete_request_cb, NULL);
+
+	/* Conformant */
+	ad->conform = elm_conformant_add(ad->win);
+	evas_object_size_hint_weight_set(ad->conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	elm_win_resize_object_add(ad->win, ad->conform);
+	evas_object_show(ad->conform);
+
+	/* Indicator */
+	elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
+	elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_TRANSPARENT);
+
+	ecore_event_handler_add(ECORE_EVENT_KEY_DOWN, _home_key_cb, NULL);
+
+	ad->base = create_mainview(ad);
+	elm_object_content_set(ad->conform, ad->base);
+	evas_object_show(ad->win);
 
 	return 0;
 }
 
-static void __create_ui_layout(void)
-{
-	Evas_Object *win, *conform, *layout;
-
-	win = elm_win_add(NULL, "ode-app", ELM_WIN_BASIC);
-	elm_win_conformant_set(win, EINA_TRUE);
-	elm_win_autodel_set(win, EINA_TRUE);
-
-	if (elm_win_wm_rotation_supported_get(win)) {
-		int rots[4] = { 0, 90, 180, 270 };
-		elm_win_wm_rotation_available_rotations_set(win, (const int *)(&rots), 4);
-	}
-
-	evas_object_smart_callback_add(win, "delete, request", __win_delete_request_cb, NULL);
-
-	elm_win_indicator_mode_set(win, ELM_WIN_INDICATOR_SHOW);
-	elm_win_indicator_opacity_set(win, ELM_WIN_INDICATOR_OPAQUE);
-
-	conform = elm_conformant_add(win);
-	evas_object_size_hint_weight_set(conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	elm_win_resize_object_add(win, conform);
-	evas_object_show(conform);
-
-	layout = elm_layout_add(conform);
-	evas_object_size_hint_weight_set(layout, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	elm_layout_theme_set(layout, "layout", "application", "default");
-	elm_object_content_set(conform, layout);
-	evas_object_show(layout);
-
-	evas_object_show(win);
-
-	UG_INIT_EFL(win, UG_OPT_INDICATOR_ENABLE);
-
-	return;
-}
-
-static bool __app_create(void *data)
+static bool app_create(void* data)
 {
 	return true;
 }
 
-static void __app_control(app_control_h app_control, void *data)
+static void app_control(app_control_h app_control, void* data)
 {
-	char *ug_type = (char *) data;
-	int ret = 0;
+	/* Handle the launch request. */
+	dlog_print(DLOG_ERROR, LOG_TAG, "### app create start ###");
+	appdata_s* ad = &global_ad;
+	int ret;
+	char *viewtype = NULL;
 
-	ret = app_control_get_extra_data(app_control, "viewtype", &ug_type);
-	if (ret != APP_CONTROL_ERROR_NONE)
-		dlog_print(DLOG_ERROR, LOG_TAG, "failed to get bundle data. error:%d", ret);
+	ret = app_control_get_extra_data(app_control, "viewtype", &viewtype);
+	if (ret != APP_CONTROL_ERROR_NONE) {
+		dlog_print(DLOG_ERROR, LOG_TAG, "failed to get viewtype");
+		ui_app_exit();
+	}
+
+	dlog_print(DLOG_DEBUG, LOG_TAG, "viewtype is %s", viewtype);
+	if (!strcmp(viewtype, "ENCRYPT_DEVICE"))
+		ad->view_type = ENCRYPT_DEVICE;
+	else if (!strcmp(viewtype, "DECRYPT_DEVICE"))
+		ad->view_type = DECRYPT_DEVICE;
+	else if (!strcmp(viewtype, "ENCRYPT_SD_CARD"))
+		ad->view_type = ENCRYPT_SD_CARD;
+	else if (!strcmp(viewtype, "DECRYPT_SD_CARD"))
+		ad->view_type = DECRYPT_SD_CARD;
 
 	elm_app_base_scale_set(1.8);
 
-	__create_ui_layout();
+	if (create_base_gui(ad) != 0) {
+		dlog_print(DLOG_ERROR, LOG_TAG, "### create base gui fail ###");
+		return;
+	}
+}
 
-	if (!strcmp(ug_type, "encryption"))
-		ret = __create_ug(ENCRYPTION_UG);
-	else if (!strcmp(ug_type, "decryption"))
-		ret = __create_ug(DECRYPTION_UG);
+static void app_pause(void* data)
+{
+	/* Take necessary actions when application becomes invisible. */
+}
 
+static void app_resume(void* data)
+{
+	/* Take necessary actions when application becomes visible. */
+}
+
+static void app_terminate(void* data)
+{
+	/* unregister vconf notify callback */
+	int ret;
+
+	ret = vconf_ignore_key_changed(VCONFKEY_SETAPPL_SCREEN_LOCK_TYPE_INT, locktype_status_changed_cb);
 	if (ret != 0)
-		ui_app_exit();
+		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to unregister lockscreen type callback");
+
+	ret = vconf_ignore_key_changed(VCONFKEY_SYSMAN_MMC_STATUS, sdcard_status_changed_cb);
+	if (ret != 0)
+		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to unregister sdcard callback");
+
+	/* unregister runtime info callback */
+	ret = runtime_info_unset_changed_cb(RUNTIME_INFO_KEY_CHARGER_CONNECTED);
+	if (ret != 0)
+		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to unregister runtime info callback");
+
+	/* unregister battery percent callback */
+	ret = device_remove_callback(DEVICE_CALLBACK_BATTERY_CAPACITY, battery_changed_cb);
+	if (ret != 0)
+		dlog_print(DLOG_ERROR, LOG_TAG, "Failed to unregister battery capacity callback");
+}
+
+static void ui_app_lang_changed(app_event_info_h event_info, void* user_data)
+{
+	/* APP_EVENT_LANGUAGE_CHANGED */
+	char* locale = NULL;
+
+	system_settings_get_value_string(SYSTEM_SETTINGS_KEY_LOCALE_LANGUAGE, &locale);
+	elm_language_set(locale);
+	free(locale);
 
 	return;
 }
 
-static void __app_pause(void *data)
+static void ui_app_orient_changed(app_event_info_h event_info, void* user_data)
 {
+	/*APP_EVENT_DEVICE_ORIENTATION_CHANGED */
 	return;
 }
 
-static void __app_resume(void *data)
+static void ui_app_region_changed(app_event_info_h event_info, void* user_data)
 {
-	return;
+	/*APP_EVENT_REGION_FORMAT_CHANGED */
 }
 
-static void __app_terminate(void *data)
+static void ui_app_low_battery(app_event_info_h event_info, void* user_data)
 {
-	return;
+	/*APP_EVENT_LOW_BATTERY */
 }
 
-int main(int argc, char *argv[])
+static void ui_app_low_memory(app_event_info_h event_info, void* user_data)
 {
-	char ug_name[1024] = "\0";
+	/*APP_EVENT_LOW_MEMORY */
+}
+
+int main(int argc, char* argv[])
+{
 	int ret = 0;
 
 	ui_app_lifecycle_callback_s event_callback = { 0, };
+	app_event_handler_h handlers[5] = { NULL, };
 
-	event_callback.create = __app_create;
-	event_callback.terminate = __app_terminate;
-	event_callback.pause = __app_pause;
-	event_callback.resume = __app_resume;
-	event_callback.app_control = __app_control;
+	event_callback.create = app_create;
+	event_callback.terminate = app_terminate;
+	event_callback.pause = app_pause;
+	event_callback.resume = app_resume;
+	event_callback.app_control = app_control;
 
-	ret = ui_app_main(argc, argv, &event_callback, ug_name);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY], APP_EVENT_LOW_BATTERY, ui_app_low_battery, &global_ad);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY], APP_EVENT_LOW_MEMORY, ui_app_low_memory, &global_ad);
+	ui_app_add_event_handler(&handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED], APP_EVENT_DEVICE_ORIENTATION_CHANGED, ui_app_orient_changed, &global_ad);
+	ui_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED], APP_EVENT_LANGUAGE_CHANGED, ui_app_lang_changed, &global_ad);
+	ui_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED], APP_EVENT_REGION_FORMAT_CHANGED, ui_app_region_changed, &global_ad);
+	ui_app_remove_event_handler(handlers[APP_EVENT_LOW_MEMORY]);
+
+	ret = ui_app_main(argc, argv, &event_callback, &global_ad);
 	if (ret != APP_ERROR_NONE)
-		dlog_print(DLOG_ERROR, LOG_TAG, "ui_app_main() is failed. err = %d", ret);
+		dlog_print(DLOG_ERROR, LOG_TAG, "ui_app_main is failed. err = %d", ret);
+	return ret;
 
-	return 0;
 }
