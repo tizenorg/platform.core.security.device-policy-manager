80c0f0a370b1b549871f8fd24794aa959038fe2a
Sungbae Yoo <sungbae.yoo@samsung.com>
2016-06-28 18:20:08 +0900

    Change kaskit to be launched in the zone
    Signed-off-by: Sungbae Yoo <sungbae.yoo@samsung.com>
    Change-Id: I0dd5b0d0618d38b57188e1fe5b96b95c42b09194

--- zone/setup-wizard/include/zone-setup.h
+++ zone/setup-wizard/include/zone-setup.h
@@ -29,7 +29,9 @@
 #include <dlog.h>
 #include <Elementary.h>
 #include <efl_extension.h>
+#include <shortcut_manager.h>
 #include <zone/zone.h>
+#include <zone/app-proxy.h>
 
 #include "setup-text.h"
 
@@ -41,6 +43,8 @@
 #if !defined(PACKAGE)
 #define PACKAGE "org.tizen.zone-setup-wizard"
 #endif
+
+#define KASKIT_PACKAGE "org.tizen.kaskit"
 
 #define DPM_SYSPOPUP_ICON_PATH "/usr/share/icons/default/small/org.tizen.dpm-syspopup.png"
 
