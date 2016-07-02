2f544c8587e1c01108c0ab40fae2925a457cc1eb
Jaemin Ryu <jm77.ryu@samsung.com>
2016-07-02 15:23:51 +0900

    Add API test cases
    Change-Id: Ide2f963c7a6fdfd0028f3e0c7bba1e2747af9a74
    Signed-off-by: Jaemin Ryu <jm77.ryu@samsung.com>

--- tests/api/password.c
+++ tests/api/password.c
@@ -24,354 +24,348 @@
 
 static int password_set_password_quality(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_password_quality(handle, "owner", DPM_PASSWORD_QUALITY_ALPHANUMERIC) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_quality(handle, DPM_PASSWORD_QUALITY_ALPHANUMERIC) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_password_minimum_length(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_password_minimum_length(handle, "owner", 27) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_minimum_length(handle, 27) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_min_password_complex_chars(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_min_password_complex_chars(handle, "owner", 10) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_min_complex_chars(handle, 10) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_maximum_failed_password_for_wipe(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_maximum_failed_password_for_wipe(handle, "owner", 11) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_maximum_failed_attempts_for_wipe(handle, 11) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_password_expires(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_password_expires(handle, "owner", 30) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_expires(handle, 30) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_password_history(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_password_history(handle, "owner", 12) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_history(handle, 12) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_password_pattern(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_password_pattern(handle, "owner", "[a-zA-Z]{4}[0-9]{4}") == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_pattern(handle, "[a-zA-Z]{4}[0-9]{4}") == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_reset_password(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_reset_password(handle, "owner", "tizen") == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_reset(handle, "tizen") == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_enforce_password_change(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_enforce_password_change(handle, "owner") == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_enforce_change(handle) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_max_inactivity_time_device_lock(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_max_inactivity_time_device_lock(handle, "owner", 30) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_max_inactivity_time_device_lock(handle, 30) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_get_max_inactivity_time_device_lock(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
     int max_inactivity_time = 0;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_get_max_inactivity_time_device_lock(handle, "owner", &max_inactivity_time) == 0) {
-        dpm_context_destroy(handle);
-        printf("[DPM] maxInactivityTime: %d\n", max_inactivity_time);
+    if (dpm_password_get_max_inactivity_time_device_lock(handle, &max_inactivity_time) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_password_status(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_password_status(handle, "owner", 10) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_status(handle, 10) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_delete_password_pattern(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_delete_password_pattern(handle, "owner") == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_delete_pattern(handle) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_get_password_pattern(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
     char *passwd_pattern;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    passwd_pattern = dpm_get_password_pattern(handle, "owner");
+    dpm_password_get_pattern(handle, &passwd_pattern);
     if (passwd_pattern != NULL) {
-        printf("[DPM] PasswordPolicy Pattern: %s\n", passwd_pattern);
-
 		free(passwd_pattern);
-
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_maximum_character_occurrences(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_maximum_character_occurrences(handle, "owner", 15) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_maximum_character_occurrences(handle, 15) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_get_maximum_character_occurrences(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
     int maximum_char_occurences = 0;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_get_maximum_character_occurrences(handle, "owner", &maximum_char_occurences) == 0) {
-        dpm_context_destroy(handle);
-        printf("[DPM] maxCharOccurences: %d\n", maximum_char_occurences);
+    if (dpm_password_get_maximum_character_occurrences(handle, &maximum_char_occurences) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_set_maximum_numeric_sequence_length(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_set_maximum_numeric_sequence_length(handle, "owner", 7) == 0) {
-        dpm_context_destroy(handle);
+    if (dpm_password_set_maximum_numeric_sequence_length(handle, 7) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
 static int password_get_maximum_numeric_sequence_length(struct testcase *tc)
 {
-    dpm_context_h handle;
+    device_policy_manager_h handle;
     int maximum_num_seq_length = 0;
 
-    handle = dpm_context_create();
+    handle = dpm_manager_create();
     if (handle == NULL) {
         printf("Failed to create client handle\n");
         return TEST_FAILED;
     }
 
-    if (dpm_get_maximum_numeric_sequence_length(handle, "owner", &maximum_num_seq_length) == 0) {
-        dpm_context_destroy(handle);
-        printf("[DPM] maxNumSeqLength: %d\n", maximum_num_seq_length);
+    if (dpm_password_get_maximum_numeric_sequence_length(handle, &maximum_num_seq_length) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
-    dpm_context_destroy(handle);
+    dpm_manager_destroy(handle);
     return TEST_FAILED;
 }
 
@@ -486,4 +480,3 @@ void TESTCASE_CONSTRUCTOR password_policy_build_testcase(void)
     testbench_populate_testcase(&security_testcase_set_maximum_numeric_sequence_length);
     testbench_populate_testcase(&security_testcase_reset_password);
 }
-
