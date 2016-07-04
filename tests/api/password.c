829a252359c75fbef7d36ee01c9bdcdde1ef6551
Sungjun, Lee <sjun221.lee@samsung.com>
2016-07-04 17:07:57 +0900

    Add Password API test cases
    Change-Id: I40e9e5c3952950493eda015becd8d842cb4c86e1
    Signed-off-by: Sungjun, Lee <sjun221.lee@samsung.com>

--- tests/api/password.c
+++ tests/api/password.c
@@ -32,7 +32,47 @@ static int password_set_password_quality(struct testcase *tc)
         return TEST_FAILED;
     }
 
-    if (dpm_password_set_quality(handle, DPM_PASSWORD_QUALITY_ALPHANUMERIC) == 0) {
+    if (dpm_password_set_quality(handle, DPM_PASSWORD_QUALITY_UNSPECIFIED) != 0) {
+        dpm_manager_destroy(handle);
+        return TEST_FAILED;
+    }
+    if (dpm_password_set_quality(handle, DPM_PASSWORD_QUALITY_SIMPLE_PASSWORD) != 0) {
+        dpm_manager_destroy(handle);
+        return TEST_FAILED;
+    }
+    if (dpm_password_set_quality(handle, DPM_PASSWORD_QUALITY_SOMETHING) != 0) {
+        dpm_manager_destroy(handle);
+        return TEST_FAILED;
+    }
+    if (dpm_password_set_quality(handle, DPM_PASSWORD_QUALITY_NUMERIC) != 0) {
+        dpm_manager_destroy(handle);
+        return TEST_FAILED;
+    }
+    if (dpm_password_set_quality(handle, DPM_PASSWORD_QUALITY_ALPHABETIC) != 0) {
+        dpm_manager_destroy(handle);
+        return TEST_FAILED;
+    }
+    if (dpm_password_set_quality(handle, DPM_PASSWORD_QUALITY_ALPHANUMERIC) != 0) {
+        dpm_manager_destroy(handle);
+        return TEST_FAILED;
+    }
+
+    dpm_manager_destroy(handle);
+    return TEST_SUCCESSED;
+}
+
+static int password_get_password_quality(struct testcase *tc)
+{
+    device_policy_manager_h handle;
+    dpm_password_quality_e quality = DPM_PASSWORD_QUALITY_UNSPECIFIED;
+
+    handle = dpm_manager_create();
+    if (handle == NULL) {
+        printf("Failed to create client handle\n");
+        return TEST_FAILED;
+    }
+
+    if (dpm_password_get_quality(handle, &quality) == 0) {
         dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
@@ -60,6 +100,26 @@ static int password_set_password_minimum_length(struct testcase *tc)
     return TEST_FAILED;
 }
 
+static int password_get_password_minimum_length(struct testcase *tc)
+{
+    device_policy_manager_h handle;
+    int min_length = 0;
+
+    handle = dpm_manager_create();
+    if (handle == NULL) {
+        printf("Failed to create client handle\n");
+        return TEST_FAILED;
+    }
+
+    if (dpm_password_get_minimum_length(handle, &min_length) == 0) {
+        dpm_manager_destroy(handle);
+        return TEST_SUCCESSED;
+    }
+
+    dpm_manager_destroy(handle);
+    return TEST_FAILED;
+}
+
 static int password_set_min_password_complex_chars(struct testcase *tc)
 {
     device_policy_manager_h handle;
@@ -71,6 +131,26 @@ static int password_set_min_password_complex_chars(struct testcase *tc)
     }
 
     if (dpm_password_set_min_complex_chars(handle, 10) == 0) {
+        dpm_manager_destroy(handle);
+        return TEST_SUCCESSED;
+    }
+
+    dpm_manager_destroy(handle);
+    return TEST_FAILED;
+}
+
+static int password_get_min_password_complex_chars(struct testcase *tc)
+{
+    device_policy_manager_h handle;
+    int min_complex_chars = 0;
+
+    handle = dpm_manager_create();
+    if (handle == NULL) {
+        printf("Failed to create client handle\n");
+        return TEST_FAILED;
+    }
+
+    if (dpm_password_get_min_complex_chars(handle, &min_complex_chars) == 0) {
         dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
@@ -98,6 +178,26 @@ static int password_set_maximum_failed_password_for_wipe(struct testcase *tc)
     return TEST_FAILED;
 }
 
+static int password_get_maximum_failed_password_for_wipe(struct testcase *tc)
+{
+    device_policy_manager_h handle;
+    int max_failed_attempts = 0;
+
+    handle = dpm_manager_create();
+    if (handle == NULL) {
+        printf("Failed to create client handle\n");
+        return TEST_FAILED;
+    }
+
+    if (dpm_password_get_maximum_failed_attempts_for_wipe(handle, &max_failed_attempts) == 0) {
+        dpm_manager_destroy(handle);
+        return TEST_SUCCESSED;
+    }
+
+    dpm_manager_destroy(handle);
+    return TEST_FAILED;
+}
+
 static int password_set_password_expires(struct testcase *tc)
 {
     device_policy_manager_h handle;
@@ -109,6 +209,26 @@ static int password_set_password_expires(struct testcase *tc)
     }
 
     if (dpm_password_set_expires(handle, 30) == 0) {
+        dpm_manager_destroy(handle);
+        return TEST_SUCCESSED;
+    }
+
+    dpm_manager_destroy(handle);
+    return TEST_FAILED;
+}
+
+static int password_get_password_expires(struct testcase *tc)
+{
+    device_policy_manager_h handle;
+    int expires = 0;
+
+    handle = dpm_manager_create();
+    if (handle == NULL) {
+        printf("Failed to create client handle\n");
+        return TEST_FAILED;
+    }
+
+    if (dpm_password_get_expires(handle, &expires) == 0) {
         dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
@@ -136,9 +256,10 @@ static int password_set_password_history(struct testcase *tc)
     return TEST_FAILED;
 }
 
-static int password_set_password_pattern(struct testcase *tc)
+static int password_get_password_history(struct testcase *tc)
 {
     device_policy_manager_h handle;
+    int history = 0;
 
     handle = dpm_manager_create();
     if (handle == NULL) {
@@ -146,7 +267,7 @@ static int password_set_password_pattern(struct testcase *tc)
         return TEST_FAILED;
     }
 
-    if (dpm_password_set_pattern(handle, "[a-zA-Z]{4}[0-9]{4}") == 0) {
+    if (dpm_password_get_history(handle, &history) == 0) {
         dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
@@ -235,6 +356,7 @@ static int password_get_max_inactivity_time_device_lock(struct testcase *tc)
 static int password_set_password_status(struct testcase *tc)
 {
     device_policy_manager_h handle;
+    dpm_password_status_e status = DPM_PASSWORD_STATUS_NORMAL;
 
     handle = dpm_manager_create();
     if (handle == NULL) {
@@ -242,16 +364,22 @@ static int password_set_password_status(struct testcase *tc)
         return TEST_FAILED;
     }
 
-    if (dpm_password_set_status(handle, 10) == 0) {
+    status = DPM_PASSWORD_STATUS_CHANGE_REQUIRED;
+    if (dpm_password_set_status(handle, status) != 0) {
         dpm_manager_destroy(handle);
-        return TEST_SUCCESSED;
+        return TEST_FAILED;
+    }
+    status = DPM_PASSWORD_STATUS_CHANGED;
+    if (dpm_password_set_status(handle, status) != 0) {
+        dpm_manager_destroy(handle);
+        return TEST_FAILED;
     }
 
     dpm_manager_destroy(handle);
-    return TEST_FAILED;
+    return TEST_SUCCESSED;
 }
 
-static int password_delete_password_pattern(struct testcase *tc)
+static int password_set_password_pattern(struct testcase *tc)
 {
     device_policy_manager_h handle;
 
@@ -261,7 +389,7 @@ static int password_delete_password_pattern(struct testcase *tc)
         return TEST_FAILED;
     }
 
-    if (dpm_password_delete_pattern(handle) == 0) {
+    if (dpm_password_set_pattern(handle, "[a-zA-Z]{4}[0-9]{4}") == 0) {
         dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
@@ -283,7 +411,26 @@ static int password_get_password_pattern(struct testcase *tc)
 
     dpm_password_get_pattern(handle, &passwd_pattern);
     if (passwd_pattern != NULL) {
-		free(passwd_pattern);
+        free(passwd_pattern);
+        return TEST_SUCCESSED;
+    }
+
+    dpm_manager_destroy(handle);
+    return TEST_FAILED;
+}
+
+static int password_delete_password_pattern(struct testcase *tc)
+{
+    device_policy_manager_h handle;
+
+    handle = dpm_manager_create();
+    if (handle == NULL) {
+        printf("Failed to create client handle\n");
+        return TEST_FAILED;
+    }
+
+    if (dpm_password_delete_pattern(handle) == 0) {
+        dpm_manager_destroy(handle);
         return TEST_SUCCESSED;
     }
 
@@ -374,9 +521,19 @@ struct testcase security_testcase_set_password_quality = {
     .handler = password_set_password_quality
 };
 
+struct testcase security_testcase_get_password_quality = {
+    .description = "dpm_get_password_quality",
+    .handler = password_get_password_quality
+};
+
 struct testcase security_testcase_set_password_minimum_length = {
     .description = "dpm_set_password_minimum_length",
     .handler = password_set_password_minimum_length
+};
+
+struct testcase security_testcase_get_password_minimum_length = {
+    .description = "dpm_get_password_minimum_length",
+    .handler = password_get_password_minimum_length
 };
 
 struct testcase security_testcase_set_min_password_complex_chars = {
@@ -384,9 +541,19 @@ struct testcase security_testcase_set_min_password_complex_chars = {
     .handler = password_set_min_password_complex_chars
 };
 
+struct testcase security_testcase_get_min_password_complex_chars = {
+    .description = "dpm_get_min_password_complex_chars",
+    .handler = password_get_min_password_complex_chars
+};
+
 struct testcase security_testcase_set_maximum_failed_password_for_wipe = {
     .description = "dpm_set_maximum_failed_password_for_wipe",
     .handler = password_set_maximum_failed_password_for_wipe
+};
+
+struct testcase security_testcase_get_maximum_failed_password_for_wipe = {
+    .description = "dpm_get_maximum_failed_password_for_wipe",
+    .handler = password_get_maximum_failed_password_for_wipe
 };
 
 struct testcase security_testcase_set_password_expires = {
@@ -394,14 +561,19 @@ struct testcase security_testcase_set_password_expires = {
     .handler = password_set_password_expires
 };
 
+struct testcase security_testcase_get_password_expires = {
+    .description = "dpm_get_password_expires",
+    .handler = password_get_password_expires
+};
+
 struct testcase security_testcase_set_password_history = {
     .description = "dpm_set_password_history",
     .handler = password_set_password_history
 };
 
-struct testcase security_testcase_set_password_pattern = {
-    .description = "dpm_set_password_pattern",
-    .handler = password_set_password_pattern
+struct testcase security_testcase_get_password_history = {
+    .description = "dpm_get_password_history",
+    .handler = password_get_password_history
 };
 
 struct testcase security_testcase_reset_password = {
@@ -429,14 +601,19 @@ struct testcase security_testcase_set_password_status = {
     .handler = password_set_password_status
 };
 
-struct testcase security_testcase_delete_password_pattern = {
-    .description = "dpm_delete_password_pattern",
-    .handler = password_delete_password_pattern
+struct testcase security_testcase_set_password_pattern = {
+    .description = "dpm_set_password_pattern",
+    .handler = password_set_password_pattern
 };
 
 struct testcase security_testcase_get_password_pattern = {
     .description = "dpm_get_password_pattern",
     .handler = password_get_password_pattern
+};
+
+struct testcase security_testcase_delete_password_pattern = {
+    .description = "dpm_delete_password_pattern",
+    .handler = password_delete_password_pattern
 };
 
 struct testcase security_testcase_set_maximum_character_occurrences = {
@@ -461,11 +638,17 @@ struct testcase security_testcase_get_maximum_numeric_sequence_length = {
 
 void TESTCASE_CONSTRUCTOR password_policy_build_testcase(void)
 {
+    testbench_populate_testcase(&security_testcase_get_password_quality);
     testbench_populate_testcase(&security_testcase_set_password_quality);
+    testbench_populate_testcase(&security_testcase_get_password_minimum_length);
     testbench_populate_testcase(&security_testcase_set_password_minimum_length);
+    testbench_populate_testcase(&security_testcase_get_min_password_complex_chars);
     testbench_populate_testcase(&security_testcase_set_min_password_complex_chars);
+    testbench_populate_testcase(&security_testcase_get_maximum_failed_password_for_wipe);
     testbench_populate_testcase(&security_testcase_set_maximum_failed_password_for_wipe);
+    testbench_populate_testcase(&security_testcase_get_password_expires);
     testbench_populate_testcase(&security_testcase_set_password_expires);
+    testbench_populate_testcase(&security_testcase_get_password_history);
     testbench_populate_testcase(&security_testcase_set_password_history);
     testbench_populate_testcase(&security_testcase_enforce_password_change);
     testbench_populate_testcase(&security_testcase_get_max_inactivity_time_device_lock);
